cbaa778776e39d271b8cb0c3aa01e30609f710d9
Antti Hyvarinen <antti.hyvarinen@gmail.com>
2013-01-21 11:52:32 +0000

    intermediate...
    git-svn-id: https://scm.ti-edu.ch/reposvn/opensmt2/opensmt-dev/TRUNK@38 d17752b6-1f7d-11e2-a792-abcf70c528dd

--- src/egraph/test.C
+++ src/egraph/test.C
@@ -1,9 +1,23 @@
+#include "Sort.h"
+#include "SStore.h"
+#include "TStore.h"
+#include "Term.h"
+#include "PtStore.h"
+#include "Pterm.h"
+#include "Logic.h"
 #include "Enode.h"
 #include "EnodeStore.h"
-#include "Term.h"
 
 int main(int argc, char **argv) {
+    SMTConfig cfg(argc, argv);
     EnodeStore estore;
+    SStore sort_store(cfg);
+    TStore sym_store;
+    Logic logic(cfg, sort_store, sym_store);
+    PtStore term_store(sym_store, sort_store, logic.getSym_true, logic.getSym_false);
+
+    Identifier i("TSort");
+
     ERef er = estore.addSymb(TRef_Undef);
     estore.addTerm(er);
     return 0;
