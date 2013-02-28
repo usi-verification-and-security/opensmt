3c33bef04520c08636b041f0231faed1ecf6cb86
Antti Hyvarinen <antti.hyvarinen@gmail.com>
2013-02-28 16:53:41 +0000

    Added a missing file
    git-svn-id: https://scm.ti-edu.ch/reposvn/opensmt2/opensmt-dev/TRUNK@62 d17752b6-1f7d-11e2-a792-abcf70c528dd

---
+++ src/cnfizers/TermMapper.C
@@ -0,0 +1,26 @@
+#include "TermMapper.h"
+
+void TermMapper::getTerm(PTRef r, PTRef& p, bool& sgn) const {
+    sgn = false;
+    while (logic.term_store[r].symb() == logic.getSym_not()) {
+        r = logic.term_store[r][0];
+        sgn = !sgn;
+    }
+    p = r;
+}
+
+Lit TermMapper::getLit(PTRef r) const {
+    bool sgn;
+    PTRef p;
+    getTerm(r, p, sgn);
+    return Lit(termToVar[p], sgn);
+}
+
+Var TermMapper::getVar(PTRef r) const {
+    bool sgn;
+    PTRef p;
+    getTerm(r, p, sgn);
+    return termToVar[p];
+}
+
+
