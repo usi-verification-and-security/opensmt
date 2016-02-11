b7ca7bd122561cb90c0331ebb004a721b5a94abf
Matteo Marescotti <matteo.marescotti@usi.ch>
2016-02-11 09:52:31 +0000

    parallel part compiles on osx
    git-svn-id: https://scm.ti-edu.ch/reposvn/opensmt2/opensmt-dev/trunk@576 d17752b6-1f7d-11e2-a792-abcf70c528dd

--- parallel/clause_sharing/src/lib/Thread.h
+++ parallel/clause_sharing/src/lib/Thread.h
@@ -14,6 +14,55 @@
 #include "Frame.h"
 
 
+#ifdef __APPLE__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if !defined(PTHREAD_BARRIER_SERIAL_THREAD)
+# define PTHREAD_BARRIER_SERIAL_THREAD  (1)
+#endif
+
+#if !defined(PTHREAD_PROCESS_PRIVATE)
+# define PTHREAD_PROCESS_PRIVATE    (42)
+#endif
+#if !defined(PTHREAD_PROCESS_SHARED)
+# define PTHREAD_PROCESS_SHARED     (43)
+#endif
+
+typedef struct {
+} pthread_barrierattr_t;
+
+typedef struct {
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    unsigned int limit;
+    unsigned int count;
+    unsigned int phase;
+} pthread_barrier_t;
+
+int pthread_barrierattr_init(pthread_barrierattr_t *attr);
+int pthread_barrierattr_destroy(pthread_barrierattr_t *attr);
+
+int pthread_barrierattr_getpshared(const pthread_barrierattr_t *attr,
+                                   int *pshared);
+int pthread_barrierattr_setpshared(pthread_barrierattr_t *attr,
+                                   int pshared);
+
+int pthread_barrier_init(pthread_barrier_t * barrier,
+                         const pthread_barrierattr_t * attr,
+                         unsigned int count);
+int pthread_barrier_destroy(pthread_barrier_t *barrier);
+
+int pthread_barrier_wait(pthread_barrier_t *barrier);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif /* __APPLE__ */
+
 //class ThreadException : public Exception {
 //public:
 //    explicit ThreadException(const char *message) : Exception(message) { }
