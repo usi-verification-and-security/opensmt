36fffe6a76a2ad1975eb444112c3ae013d1fda3f
Antti Hyvarinen <antti.hyvarinen@gmail.com>
2016-06-21 10:06:02 +0200

    Memory for dag traversal to avoid checking shared tree branches several times

--- src/logics/LRATheory.C
+++ src/logics/LRATheory.C
@@ -4,6 +4,7 @@ bool LRATheory::simplify(vec<PushFrame>& formulas, int curr)
 {
     assert(curr < formulas.size());
     vec<PTRef> coll_f_args;
+
     // compute coll_f as (a_1^0 /\ ... /\ a_{n_1}^0) /\ ... /\ (a_1^{curr} /\ ... /\ a_{n_k}^{curr})
     for (int i = 0; i < curr+1; i++)
     {
@@ -12,8 +13,6 @@ bool LRATheory::simplify(vec<PushFrame>& formulas, int curr)
     }
     PTRef coll_f = getLogic().mkAnd(coll_f_args);
 
-//    PTRef root_with_ites;
-//    lralogic.conjoinItes(coll_f, root_with_ites);
     computeSubstitutions(coll_f, formulas[curr]);
     PTRef tmp;
     lralogic.simplifyAndSplitEq(formulas[curr].root, tmp);
