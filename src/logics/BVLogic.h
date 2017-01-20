bc912d71a47c9f1ce4988bb069048349ef70c2ae
Antti Hyvarinen <antti.hyvarinen@gmail.com>
2017-01-20 17:54:23 +0100

    Fixed the problem of ambiguity on mkConst on BV by adding assertion failure and a new function BVLogic::mkBVConst.

--- src/logics/BVLogic.h
+++ src/logics/BVLogic.h
@@ -96,7 +96,8 @@ class BVLogic: public CUFLogic
     virtual const Logic_t getLogic() const { return opensmt::QF_BV; }
 
 //    virtual PTRef         insertTerm(SymRef sym, vec<PTRef>& terms, char** msg);
-    virtual PTRef         mkConst   (const int c) { assert(c >= 0); char* num; opensmt::uwordToBinary(c, num); PTRef tr = Logic::mkConst(sort_BVNUM, num); free(num); return tr; } // Should be converting the int c to binary...
+    virtual PTRef mkConst(const int c) { assert(false); return PTRef_Undef; } // Not allowed (ambiguous)
+    PTRef         mkBVConst   (const int c) { assert(c >= 0); char* num; opensmt::uwordToBinary(c, num); PTRef tr = Logic::mkConst(sort_BVNUM, num); free(num); return tr; } // Should be converting the int c to binary...
     virtual PTRef         mkNumVar  (const char* name) { return mkVar(sort_BVNUM, name); }
     virtual bool          isBuiltinSort(SRef sr) const { return (sr == sort_BVNUM) /*|| (sr == sort_BVSTR)*/ || Logic::isBuiltinSort(sr); }
     virtual bool          isBuiltinConstant(SymRef sr) const { return isBVNUMConst(sr) || Logic::isBuiltinConstant(sr); }
