bced21f533cbec2e4be1ce72c34df5d492251fc1
sepid <sepideh.a65@gmail.com>
2017-01-21 17:45:42 +0100

    test cases for bitwise '&'  and biwise '*'

--- examples/test4_BV.cc
+++ examples/test4_BV.cc
@@ -1,11 +1,12 @@
 /************************************************
  * Created on: Jan 17, 2017
  * Author: Sepideh Asadi
- * a=36 /\ b=45 /\ not(d=54) /\ d=a && b
+ * a=1 /\ b=1 /\ d=0 /\ d=a & b
  ************************************************/
 
 #include <opensmt/opensmt2.h>
 #include <stdio.h>
+#include <opensmt/BitBlaster.h>
 //#include <opensmt/logic.h>
 
 
@@ -22,30 +23,96 @@ main(int argc, char** argv)
     // BVLogic bvlogic(c);
 
     PTRef a = logic.mkNumVar("a");
-    PTRef thirtysix = logic.mkConst(36);
-    PTRef eq1 = logic.mkEq(a, thirtysix);
+    PTRef const1 = logic.mkConst(1);
+    PTRef eq1 = logic.mkBVEq(a, const1);
 
     PTRef b = logic.mkNumVar("b");
-    PTRef fortyfive = logic.mkConst(45);
-    PTRef eq2 = logic.mkEq(a, fortyfive);
+    PTRef const2 = logic.mkConst(1);
+    PTRef eq2 = logic.mkBVEq(b, const2);
 
     PTRef d = logic.mkNumVar("d");
-    PTRef fiftyfour = logic.mkConst(54);
-    PTRef eq3 = logic.mkEq(a, fiftyfour);
-    PTRef eq3_neg = logic.mkNot(eq3);
+    PTRef const3 = logic.mkConst(0);
+    PTRef eq3 = logic.mkBVEq(d, const3);
+    //PTRef eq3_neg = logic.mkBVNot(eq3);
 
     PTRef d2 = logic.mkBVBwAnd(a, b);
-    PTRef eq4 = logic.mkEq(d, d2);
+    PTRef eq4 = logic.mkBVEq(d, d2);
 
-    vec<PTRef> args;
+
+   /* vec<PTRef> args;
     args.push(eq1);
     args.push(eq2);
     args.push(eq3_neg);
     args.push(eq4);
 
-    PTRef eq = logic.mkAnd(args);
+    PTRef eq = logic.mkAnd(args);*/
 
-    mainSolver.push(eq);
+    SolverId id = { 5 };
+	vec<PtAsgn> asgns;
+	vec<DedElem> deds;
+	vec<PTRef> foo;
+
+	BitBlaster bbb(id, c, mainSolver, logic, asgns, deds, foo);
+
+	BVRef output1;
+	lbool stat;
+	stat = bbb.insertEq(eq1, output1);
+	if (stat == l_True)
+		printf("sat after eq\n");
+	if (stat == l_False)
+		printf("unsat after eq\n");
+
+	BVRef output2;
+	stat = bbb.insertEq(eq2, output2);
+//	if (stat == l_True)
+//		printf("sat after eq_neg\n");
+//	if (stat == l_False)
+//		printf("unsat after eq_neg\n");
+
+	BVRef output3;
+	stat = bbb.insertEq(eq3, output3);
+//	if (stat == l_True)
+//		printf("sat after eq_neg\n");
+//	if (stat == l_False)
+//		printf("unsat after eq_neg\n");
+
+	BVRef output4;
+	stat = bbb.insertEq(eq4, output4);
+//	if (stat == l_True)
+//		printf("sat after eq_neg\n");
+//	if (stat == l_False)
+//		printf("unsat after eq_neg\n");
+
+/****************************************************/
+/*	PTRef uf1 = logic.mkCUFVar("uf1");
+	PTRef uf2 = logic.mkCUFVar("uf2");
+
+	stat = bbb.glueUFtoB(uf1, output);
+	stat = bbb.glueBtoUF(output, uf1);
+	if (stat == l_True)
+		printf("sat after uf1\n");
+	if (stat == l_False)
+		printf("unsat after uf1\n");
+
+	stat = bbb.glueUFtoB(uf2, output2);
+	stat = bbb.glueBtoUF(output2, uf2);
+	if (stat == l_True)
+		printf("sat after uf2\n");
+	if (stat == l_False)
+		printf("unsat after uf2\n");
+
+	PTRef uf_eq = logic.mkEq(uf1, uf2);
+	char* msg;
+	mainSolver.insertFormula(uf_eq, &msg);
+
+    mainSolver.push(eq);*/
+
+	std::cout << logic.printTerm(eq1) << "\n";
+	std::cout << logic.printTerm(eq2) << "\n";
+	std::cout << logic.printTerm(eq3) << "\n";
+	//std::cout << logic.printTerm(eq3_neg) << "\n";
+	std::cout << logic.printTerm(eq4) << "\n";
+
     sstat r = mainSolver.check();
 
     if (r == s_True)
@@ -59,5 +126,3 @@ main(int argc, char** argv)
 
     return 0;
 }
-
-
--- examples/test5_BV.cc
+++ examples/test5_BV.cc
@@ -1,11 +1,13 @@
 /***************************************************
  * Created on: Jan 17, 2017
  * Author: Sepideh Asadi
- * a=5 /\ b=2 /\ not(d=10) /\ d=a*b
+ * a=5 /\ b=2 /\ d=0 /\ d=a*b
+ * we cannot have such not(d=10)
  ***************************************************/
 
 #include <opensmt/opensmt2.h>
 #include <stdio.h>
+#include <opensmt/BitBlaster.h>
 //#include <opensmt/BVLogic.h>
 
 
@@ -21,24 +23,63 @@ main(int argc, char** argv)
 
    // BVLogic bvlogic(c);
 
-    PTRef five = logic.mkConst(5);
-    PTRef two = logic.mkConst(2);
-    PTRef ten = logic.mkConst(10);
+    PTRef const1 = logic.mkConst(1);
+    PTRef const2 = logic.mkConst(1);
+    PTRef const3 = logic.mkConst(0);
 
     PTRef a = logic.mkNumVar("a");
     PTRef b = logic.mkNumVar("b");
     PTRef d = logic.mkNumVar("d");
 
-    PTRef eq1 = logic.mkEq(a, five);
-    PTRef eq2 = logic.mkEq(b, two);
+    PTRef eq1 = logic.mkBVEq(a, const1);
 
-    PTRef eq3 = logic.mkEq(d, ten);
-    PTRef eq3_neg = logic.mkNot(eq3);
+    PTRef eq2 = logic.mkBVEq(b, const2);
+
+    PTRef eq3 = logic.mkBVEq(d, const3);
+    //PTRef eq3_neg = logic.mkNot(eq3);
 
     PTRef d2 = logic.mkBVTimes(a, b);
-    PTRef eq4 = logic.mkEq(d, d2);
+    PTRef eq4 = logic.mkBVEq(d, d2);
+/******************************************************/
+    SolverId id = { 5 };
+	vec<PtAsgn> asgns;
+	vec<DedElem> deds;
+	vec<PTRef> foo;
 
-    vec<PTRef> args;
+	BitBlaster bbb(id, c, mainSolver, logic, asgns, deds, foo);
+
+	BVRef output1;
+	lbool stat;
+	stat = bbb.insertEq(eq1, output1);
+//	if (stat == l_True)
+//		printf("sat after eq\n");
+//	if (stat == l_False)
+//		printf("unsat after eq\n");
+
+	BVRef output2;
+	stat = bbb.insertEq(eq2, output2);
+//	if (stat == l_True)
+//		printf("sat after eq_neg\n");
+//	if (stat == l_False)
+//		printf("unsat after eq_neg\n");
+
+	BVRef output3;
+	stat = bbb.insertEq(eq3, output3);
+//	if (stat == l_True)
+//		printf("sat after eq_neg\n");
+//	if (stat == l_False)
+//		printf("unsat after eq_neg\n");
+
+	BVRef output4;
+	stat = bbb.insertEq(eq4, output4);
+//	if (stat == l_True)
+//		printf("sat after eq_neg\n");
+//	if (stat == l_False)
+//		printf("unsat after eq_neg\n");
+
+/******************************************************/
+
+ /*   vec<PTRef> args;
     args.push(eq1);
     args.push(eq2);
     args.push(eq3_neg);
@@ -46,7 +87,14 @@ main(int argc, char** argv)
 
     PTRef eq = logic.mkAnd(args);
 
-    mainSolver.push(eq);
+    mainSolver.push(eq);*/
+
+	std::cout << logic.printTerm(eq1) << "\n";
+	std::cout << logic.printTerm(eq2) << "\n";
+	std::cout << logic.printTerm(eq3) << "\n";
+	//std::cout << logic.printTerm(eq3_neg) << "\n";
+	std::cout << logic.printTerm(eq4) << "\n";
+
     sstat r = mainSolver.check();
 
     if (r == s_True)
