18a8ddb276f3fc8dd8cd73634219ab2bc9c412d5
sepid <sepideh.a65@gmail.com>
2017-01-27 10:36:30 +0100

    test cases for BVLogic in C++ API

---
+++ examples/test7_BV.cc
@@ -0,0 +1,80 @@
+/************************************************
+ * Created on: Jan 25, 2017
+ * a=2 /\ b=0 /\ d=1 /\   d=((not a) || b)
+ *
+ * Insert four Equalities with InsertEq()
+ *
+zero means false
+anything else is True
+ ************************************************/
+
+#include <opensmt/opensmt2.h>
+#include <stdio.h>
+#include <opensmt/BitBlaster.h>
+
+int main(int argc, char** argv)
+{
+    SMTConfig c;
+    CUFTheory cuftheory(c);
+    THandler thandler(c, cuftheory);
+    SimpSMTSolver solver(c, thandler);
+    MainSolver mainSolver(thandler, c, &solver);
+    BVLogic& logic = cuftheory.getLogic();
+
+    // BVLogic bvlogic(c);
+
+    PTRef a = logic.mkBVNumVar("a");
+    PTRef const1 = logic.mkBVConst(2);
+    PTRef eq1 = logic.mkBVEq(a, const1);
+
+    PTRef b = logic.mkBVNumVar("b");
+    PTRef const2 = logic.mkBVConst(5);
+    PTRef eq2 = logic.mkBVEq(b, const2);
+
+    PTRef d = logic.mkBVNumVar("d");
+    PTRef const3 = logic.mkBVConst(1);
+    PTRef eq3 = logic.mkBVEq(d, const3);
+
+    PTRef a_neg = logic.mkBVNot(a);
+    PTRef LOr = logic.mkBVLor(a_neg, b);
+    PTRef eq4 = logic.mkBVEq(d, LOr);
+
+    SolverId id = { 5 };
+	vec<PtAsgn> asgns;
+	vec<DedElem> deds;
+	vec<PTRef> foo;
+
+	BitBlaster bbb(id, c, mainSolver, logic, asgns, deds, foo);
+
+	BVRef output1;
+	lbool stat;
+	stat = bbb.insertEq(eq1, output1);
+
+	BVRef output2;
+	stat = bbb.insertEq(eq2, output2);
+
+	BVRef output3;
+	stat = bbb.insertEq(eq3, output3);
+
+	BVRef output4;
+	stat = bbb.insertEq(eq4, output4);
+
+	std::cout << logic.printTerm(eq1) << "\n";
+	std::cout << logic.printTerm(eq2) << "\n";
+	std::cout << logic.printTerm(eq3) << "\n";
+	std::cout << logic.printTerm(LOr) << "\n";
+	std::cout << logic.printTerm(eq4) << "\n";
+
+    sstat r = mainSolver.check();
+
+    if (r == s_True)
+        printf("sat\n");
+    else if (r == s_False)
+        printf("unsat\n");
+    else if (r == s_Undef)
+        printf("unknown\n");
+    else
+        printf("error\n");
+
+    return 0;
+}
---
+++ examples/test9_BV.cc
@@ -0,0 +1,89 @@
+/************************************************
+ * Created on: Jan 25, 2017 , Grisha's email example
+ * ( (not a) \/ ( b = ( d + 1)))
+ * in general(without value) it is sat
+ *
+ * For Values:
+ * a=1 , b=1 , d=0   sat
+ * a=7 , b=2 , d=1  sat
+ * a=7 , b=2 , d=11  sat
+ *
+ * You are allowed only to use: InsertEq and InsertOr
+ ************************************************/
+
+#include <opensmt/opensmt2.h>
+#include <stdio.h>
+#include <opensmt/BitBlaster.h>
+
+int main(int argc, char** argv)
+{
+    SMTConfig c;
+    CUFTheory cuftheory(c);
+    THandler thandler(c, cuftheory);
+    SimpSMTSolver solver(c, thandler);
+    MainSolver mainSolver(thandler, c, &solver);
+    BVLogic& logic = cuftheory.getLogic();
+
+    // BVLogic bvlogic(c);
+
+    PTRef a = logic.mkBVNumVar("a");
+    PTRef const1 = logic.mkBVConst(7);
+    PTRef eq1 = logic.mkBVEq(a, const1);
+
+    PTRef a_neg = logic.mkBVNot(a);
+
+    PTRef b = logic.mkBVNumVar("b");
+    PTRef const2 = logic.mkBVConst(2);
+    PTRef eq2 = logic.mkBVEq(b, const2);
+
+    PTRef d = logic.mkBVNumVar("d");
+    PTRef const3 = logic.mkBVConst(11);
+    PTRef eq3 = logic.mkBVEq(d, const3);
+
+    PTRef one = logic.mkBVConst(1);
+    PTRef dplusOne = logic.mkBVPlus(d, one);
+    PTRef eq4 = logic.mkBVEq(b, dplusOne);
+
+    PTRef eq5 = logic.mkBVLor(a_neg, eq4);
+
+    SolverId id = { 5 };
+	vec<PtAsgn> asgns;
+	vec<DedElem> deds;
+	vec<PTRef> foo;
+
+	BitBlaster bbb(id, c, mainSolver, logic, asgns, deds, foo);
+
+	BVRef output1;
+	lbool stat;
+	stat = bbb.insertOr(eq5 , output1);
+
+	BVRef output2;
+	stat = bbb.insertEq(eq1, output2);
+
+	BVRef output3;
+	stat = bbb.insertEq(eq2, output3);
+
+	BVRef output4;
+	stat = bbb.insertEq(eq3, output4);
+
+//	std::cout << logic.printTerm(eq1) << "\n";
+//	std::cout << logic.printTerm(eq2) << "\n";
+//	std::cout << logic.printTerm(eq3) << "\n";
+//	//std::cout << logic.printTerm(LOr) << "\n";
+//	std::cout << logic.printTerm(eq4) << "\n";
+//	std::cout << logic.printTerm(eq5) << "\n";
+
+
+    sstat r = mainSolver.check();
+
+    if (r == s_True)
+        printf("sat\n");
+    else if (r == s_False)
+        printf("unsat\n");
+    else if (r == s_Undef)
+        printf("unknown\n");
+    else
+        printf("error\n");
+
+    return 0;
+}
