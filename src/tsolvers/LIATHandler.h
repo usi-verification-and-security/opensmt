857a36d5d6bf65851a4687759bdee7fd75192977
Parvin Sadigova <p.sadigova@outlook.com>
2018-06-10 13:40:37 +0100

    LIATHandler.h and LIATHandler.C files added and some changes in api and logic

---
+++ src/tsolvers/LIATHandler.h
@@ -0,0 +1,31 @@
+#ifndef LIATHandler_H
+#define LIATHandler_H
+
+#include "TSolverHandler.h"
+
+class LIASolver;
+class LIALogic;
+
+class LIATHandler : public TSolverHandler
+{
+  private:
+    LIALogic& logic;
+    LIASolver *liasolver;
+  public:
+    LIATHandler(SMTConfig& c, LRALogic& l, vec<DedElem>& d, TermMapper& tmap);
+    virtual ~LIATHandler();
+    virtual void fillTmpDeds(PTRef root, Map<PTRef,int,PTRefHash> &refs);
+    virtual bool assertLit_special(PtAsgn);
+    virtual Logic& getLogic();
+    virtual const Logic& getLogic() const;
+
+#ifdef PRODUCE_PROOF
+    virtual TheoryInterpolator* getTheoryInterpolator()
+    {
+        return NULL;
+    }
+    virtual PTRef getInterpolant(const ipartitions_t& mask, map<PTRef, icolor_t> *labels);
+#endif
+};
+
+#endif
