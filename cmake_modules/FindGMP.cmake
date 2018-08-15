063f4488068a9939df7c9aca64b3ccb4250cbac1
Antti Hyvarinen <antti.hyvarinen@gmail.com>
2018-08-15 11:02:51 +0200

    Revert "A suggestion for making it possible to compile gmp on the cluster"
    This reverts commit 83231377153a82ad284e7a2d5b90f9bb32ad6b10.

--- cmake_modules/FindGMP.cmake
+++ cmake_modules/FindGMP.cmake
@@ -3,42 +3,17 @@
 # GMP_INCLUDE_DIR - the GMP include directory
 # GMP_LIBRARIES - Libraries needed to use GMP
 
-if (GMP_INCLUDES AND GMP_LIBRARIES)
-    set(GMP_FIND_QUIETLY TRUE)
-endif (GMP_INCLUDES AND GMP_LIBRARIES)
-#
-#find_path(GMP_INCLUDES NAMES gmp.h PATHS ENV GMPDIR ${INCLUDE_INSTALL_DIR})
+if (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
+		# Already in cache, be silent
+		set(GMP_FIND_QUIETLY TRUE)
+endif (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
 
-MESSAGE(STATUS "I'm doing GMP stuff now")
-#/apps/gmp/6.1.0/gcc-6.1.0/include/gmp.h
-find_path(gmp_ROOT_DIR
-    NAMES include/gmp.h
-    PATHS "/apps/gmp/6.1.0/gcc-6.1.0"
-#    NAMES include/readline/readline.h
-)
-
-MESSAGE(STATUS "GMP root dir: " ${gmp_ROOT_DIR})
-
-find_path(GMP_INCLUDES
-    NAMES gmp.h
-    HINTS ${gmp_ROOT_DIR}/include
-)
-
-MESSAGE(STATUS "GMP include dir: " ${GMP_INCLUDES})
-
-find_library(GMP_LIBRARY
-    NAMES libgmp.so
-    HINTS ${gmp_ROOT_DIR}/lib
-)
-find_library(GMPXX_LIBRARY
-    NAMES libgmpxx.so
-    HINTS ${gmp_ROOT_DIR}/lib
-)
-
-MESSAGE(STATUS "GMP library: " ${GMP_LIBRARY})
-MESSAGE(STATUS "GMPXX library: " ${GMPXX_LIBRARY})
+find_path(GMP_INCLUDE_DIR NAMES gmp.h )
+find_library(GMP_LIBRARIES NAMES gmp libgmp )
+find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx )
+MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARIES} " " ${GMPXX_LIBRARIES} )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDES GMP_LIBRARY GMPXX_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARIES)
 
-mark_as_advanced(GMP_INCLUDES GMP_LIBRARY GMPXX_LIBRARY)
+mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARIES GMPXX_LIBRARIES)
