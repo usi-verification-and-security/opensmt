fac72979df0be09d84895ad17f797296d85f193b
Martin Blicha <blichm@usi.ch>
2018-11-01 10:41:01 +0100

    unit tests for new simplification

---
+++ test/unit/test_simplifyAssignment.cpp
@@ -0,0 +1,124 @@
+//
+// Created by Martin Blicha on 31.10.18.
+//
+
+#include <gtest/gtest.h>
+#include <BoolRewriting.h>
+#include "Logic.h"
+
+TEST(SimplifyUnderAssignment_test, test_Simple_Conjunction)
+{
+    SMTConfig config;
+    Logic logic{config};
+    PTRef a = logic.mkBoolVar("a");
+    PTRef b = logic.mkBoolVar("b");
+    PTRef conj = logic.mkAnd(a, logic.mkOr(
+            logic.mkNot(a),
+            b
+            ));
+    Map<PTRef,int,PTRefHash> PTRefToIncoming;
+    computeIncomingEdges(logic, conj, PTRefToIncoming);
+    PTRef res = simplifyUnderAssignment(logic, conj, PTRefToIncoming);
+    ASSERT_EQ(res, logic.mkAnd(a,b));
+}
+
+TEST(SimplifyUnderAssignment_test, test_ConjunctionToConstant)
+{
+    SMTConfig config;
+    Logic logic{config};
+    PTRef a = logic.mkBoolVar("a");
+    PTRef b = logic.mkBoolVar("b");
+    PTRef conj = logic.mkAnd(a, logic.mkAnd(logic.mkNot(a), b));
+    Map<PTRef,int,PTRefHash> PTRefToIncoming;
+    computeIncomingEdges(logic, conj, PTRefToIncoming);
+    PTRef res = simplifyUnderAssignment(logic, conj, PTRefToIncoming);
+    ASSERT_EQ(res, logic.getTerm_false());
+}
+
+TEST(SimplifyUnderAssignment_test, test_DisjunctionToConstant)
+{
+    SMTConfig config;
+    Logic logic{config};
+    PTRef a = logic.mkBoolVar("a");
+    PTRef b = logic.mkBoolVar("b");
+    PTRef disj = logic.mkOr(a, logic.mkOr(logic.mkNot(a), b));
+    Map<PTRef,int,PTRefHash> PTRefToIncoming;
+    computeIncomingEdges(logic, disj, PTRefToIncoming);
+    PTRef res = simplifyUnderAssignment(logic, disj, PTRefToIncoming);
+    ASSERT_EQ(res, logic.getTerm_true());
+}
+
+TEST(SimplifyUnderAssignment_test, test_Simple_Disjunction)
+{
+    SMTConfig config;
+    Logic logic{config};
+    PTRef a = logic.mkBoolVar("a");
+    PTRef b = logic.mkBoolVar("b");
+    PTRef disj = logic.mkOr(a, logic.mkAnd(
+            logic.mkNot(a),
+            b
+    ));
+    Map<PTRef,int,PTRefHash> PTRefToIncoming;
+    computeIncomingEdges(logic, disj, PTRefToIncoming);
+    PTRef res = simplifyUnderAssignment(logic, disj, PTRefToIncoming);
+    ASSERT_EQ(res, logic.mkOr(a,b));
+}
+
+TEST(SimplifyUnderAssignment_test, test_Do_Not_Simplify_Shared)
+{
+    SMTConfig config;
+    Logic logic{config};
+    PTRef a = logic.mkBoolVar("a");
+    PTRef b = logic.mkBoolVar("b");
+    PTRef c = logic.mkBoolVar("c");
+    PTRef conj = logic.mkAnd(a,b);
+    PTRef cdisj = logic.mkOr(c, conj);
+    PTRef top = logic.mkAnd(
+            cdisj,
+            logic.mkOr(logic.mkNot(a), conj)
+            );
+    Map<PTRef,int,PTRefHash> PTRefToIncoming;
+    computeIncomingEdges(logic, top, PTRefToIncoming);
+    PTRef res = simplifyUnderAssignment(logic, top, PTRefToIncoming);
+    ASSERT_EQ(res, top);
+}
+
+TEST(SimplifyUnderAssignment_test, test_NestedStructure)
+{
+    SMTConfig config;
+    Logic logic{config};
+    PTRef a = logic.mkBoolVar("a");
+    PTRef nota = logic.mkNot(a);
+    PTRef b = logic.mkBoolVar("b");
+    PTRef c = logic.mkBoolVar("c");
+    PTRef top = logic.mkAnd(
+            logic.mkOr(nota, logic.mkAnd(b, nota)),
+            c
+    );
+    Map<PTRef,int,PTRefHash> PTRefToIncoming;
+    computeIncomingEdges(logic, top, PTRefToIncoming);
+    PTRef res = simplifyUnderAssignment(logic, top, PTRefToIncoming);
+    ASSERT_EQ(res, logic.mkAnd(nota, c));
+}
+
+TEST(SimplifyUnderAssignemnt_test, test_BooleanEquality)
+{
+    SMTConfig config;
+    Logic logic{config};
+    PTRef a = logic.mkBoolVar("a");
+    PTRef nota = logic.mkNot(a);
+    PTRef b = logic.mkBoolVar("b");
+    PTRef c = logic.mkBoolVar("c");
+    PTRef eq = logic.mkEq(a,b);
+    vec<PTRef> args;
+    args.push(eq);
+    args.push(nota);
+    args.push(logic.mkOr(a, logic.mkNot(eq)));
+    PTRef top = logic.mkAnd(args);
+    Map<PTRef,int,PTRefHash> PTRefToIncoming;
+    computeIncomingEdges(logic, top, PTRefToIncoming);
+    PTRef res = simplifyUnderAssignment(logic, top, PTRefToIncoming);
+    ASSERT_TRUE(res == logic.getTerm_false());
+}
+
+
