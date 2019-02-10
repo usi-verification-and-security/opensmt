0d32e8ceafb3408f5eb30c10b6e4cfca2a453f51
Antti <antti.hyvarinen@gmail.com>
2019-02-10 12:29:06 +0100

    Cleaned code more

--- src/smtsolvers/LookaheadSplitter.cpp
+++ src/smtsolvers/LookaheadSplitter.cpp
@@ -20,14 +20,14 @@ LookaheadSMTSolver::LALoopRes LookaheadSplitter::solveLookahead()
     int maxDepth = getLog2Ceil(config.sat_split_num());
 
     updateRound();
-    vec<LANode*> queue;
-    LANode *root = new LANode();
+    vec<LASplitNode*> queue;
+    LASplitNode *root = new LASplitNode();
     root->p  = root;
     queue.push(root);
 
     while (queue.size() != 0)
     {
-        LANode* n = queue.last();
+        LASplitNode* n = queue.last();
         queue.pop();
 #ifdef LADEBUG
         printf("main loop: dl %d -> %d\n", decisionLevel(), 0);
@@ -41,8 +41,10 @@ LookaheadSMTSolver::LALoopRes LookaheadSplitter::solveLookahead()
                 return LALoopRes::unsat;
             case PathBuildResult::pathbuild_restart:
                 return LALoopRes::restart;
-            case PathBuildResult::pathbuild_unsat:
+            case PathBuildResult::pathbuild_unsat: {
+                // Here we need to clear all instances associated with the node n
                 continue;
+            }
             case PathBuildResult::pathbuild_success:
                 ;
         }
@@ -53,15 +55,15 @@ LookaheadSMTSolver::LALoopRes LookaheadSplitter::solveLookahead()
             printf("Producing a split:\n");;
             printTrace();
 #endif
-            createSplitLookahead();
+            createSplitLookahead(n);
             if (config.sat_split_test_cube_and_conquer())
                 return LALoopRes::unsat; // The cube-and-conquer experiment
             else
                 continue;
         }
 
-        LANode *c1 = new LANode();
-        LANode *c2 = new LANode();
+        LASplitNode *c1 = new LASplitNode();
+        LASplitNode *c2 = new LASplitNode();
         switch (expandTree(n, *c1, *c2)) {
             case laresult::la_tl_unsat:
                 return LALoopRes::unsat;
@@ -83,5 +85,36 @@ LookaheadSMTSolver::LALoopRes LookaheadSplitter::solveLookahead()
     root->print();
 #endif
     return LALoopRes::unknown;
+}
+
+bool LookaheadSplitter::createSplitLookahead(LASplitNode *n)
+{
+    // TODO: Do not push to splits, but instead to n from now on
+    int curr_dl0_idx = trail_lim.size() > 0 ? trail_lim[0] : trail.size();
+//    n->sd = new SplitData(ca, clauses, trail, curr_dl0_idx, theory_handler, config.smt_split_format_length() == spformat_brief));
+    splits.push_c(SplitData(ca, clauses, trail, curr_dl0_idx, theory_handler, config.smt_split_format_length() == spformat_brief));
+    SplitData& sp = splits.last();
+
+    printf("; Outputing an instance:\n; ");
+    Lit p = lit_Undef;
+    for (int i = 0; i < decisionLevel(); i++)
+    {
+        vec<Lit> tmp;
+        Lit l = trail[trail_lim[i]];
+        if (p != l) {
+            // In cases where the LA solver couldn't propagate due to
+            // literal being already assigned, the literal may be
+            // duplicated.  Do not report duplicates.
+            tmp.push(l);
+            printf("%s%d ", sign(l) ? "-" : "", var(l));
+            sp.addConstraint(tmp);
+        }
+        p = l;
+    }
+    printf("\n");
+
+    sp.updateInstance();
+    assert(ok);
+    return true;
 }
 
