389deca3e57e740dbeef61cc8b561b16be520c6d
Martin Blicha <blichm@usi.ch>
2019-05-06 20:07:15 +0200

    modified regresssion test script to accept path to executable

--- regression/run-test-notiming.sh
+++ regression/run-test-notiming.sh
@@ -1,10 +1,14 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 echo "This is the script for running regression tests"
 echo " - date: $(date '+%Y-%m-%d at %H:%M.%S')"
 echo " - host name $(hostname -f)"
 echo " - script path: $(readlink -f $0)"
 
 opensmt=../opensmt
+if [[ ! $# -eq 0 ]] ; then
+    #echo 'Setting path to' $1
+    opensmt=$1
+fi
 
 tmpfolder=log-$(date '+%Y-%m-%d-%H-%M-%S')
 mkdir ${tmpfolder}
@@ -27,11 +31,13 @@ for file in $(find . -name '*.smt2' |sort) generic/foo; do
     if [ $? != 0 ]; then
         echo "stdout differs for benchmark $file";
         outmod=true;
+        diff ${tmpfolder}/${name}.out ${dir}/${name}.expected.out
     fi
     diff -q ${tmpfolder}/${name}.err ${dir}/${name}.expected.err
     if [ $? != 0 ]; then
         echo "stderr differs for benchmark $file";
         errmod=true;
+        diff ${tmpfolder}/${name}.err ${dir}/${name}.expected.err
     fi
 
 done
