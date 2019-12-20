16a339f84cd44ce614ad873157f02714e788c33e
Martin Blicha <blichm@usi.ch>
2019-12-20 16:13:40 +0100

    added test detecting an error in computing the final value of delta

--- test/unit/test_Simplex.cpp
+++ test/unit/test_Simplex.cpp
@@ -100,3 +100,56 @@ TEST(Simplex_test, test_ops_in_Simplex)
     sum = s.getValuation(y_minus_x);
     cout << "y - x = " << sum.R() + sum.D() * d << endl;
 }
+
+TEST(Simplex_test, test_Assignment)
+{
+    SMTConfig c;
+    LAVarStore vs;
+
+    LVRef x = vs.getNewVar();
+    LVRef y = vs.getNewVar();
+
+    LVRef y_minus_x = vs.getNewVar();
+
+    LABoundStore bs(vs);
+
+    LABoundStore::BoundInfo x_nostrict_1 = bs.allocBoundPair(x, 1, false); // x <= 1 and x > 0
+    LABoundStore::BoundInfo y_strict_0   = bs.allocBoundPair(y, 0, true); // y < 0 and y >= 0
+
+    LABoundStore::BoundInfo x_strict_m5   = bs.allocBoundPair(x, -5, true); // x < -5 and x >= -5
+
+    LABoundStore::BoundInfo y_minus_x_strict_0    = bs.allocBoundPair(y_minus_x, 0, true);    // y - x < 0 and y - x >= 0
+    LABoundStore::BoundInfo y_minus_x_nostrict_0  = bs.allocBoundPair(y_minus_x, 0, false);   // y - x <= 0 and y - x > 0
+
+    bs.buildBounds();
+
+    Simplex s(bs);
+
+    s.newNonbasicVar(x);
+    s.newNonbasicVar(y);
+    auto p_y_plus_x = std::unique_ptr<Polynomial>(new Polynomial());
+    p_y_plus_x->addTerm(x, 1);
+    p_y_plus_x->addTerm(y, 1);
+    s.newRow(y_minus_x, std::move(p_y_plus_x));
+
+    s.initModel();
+    s.assertBoundOnVar(x_nostrict_1.v, x_nostrict_1.ub);
+    s.assertBoundOnVar(x_strict_m5.v, x_strict_m5.lb);
+    s.assertBoundOnVar(y_strict_0.v, y_strict_0.ub);
+    s.assertBoundOnVar(y_minus_x_strict_0.v, y_minus_x_strict_0.lb);
+    s.assertBoundOnVar(y_minus_x_nostrict_0.v, y_minus_x_nostrict_0.ub);
+
+    Simplex::Explanation ex = s.checkSimplex();
+    ASSERT_EQ(ex.size(), 0);
+    ASSERT_LT(s.getValuation(y), (Delta{0,0}));
+    ASSERT_LE(s.getValuation(x), (Delta{1,0}));
+    ASSERT_GE(s.getValuation(x), (Delta{-5,0}));
+    ASSERT_EQ(s.getValuation(x), -1 * s.getValuation(y));
+    Real d = s.computeDelta();
+    Real x_val = s.getValuation(x).R() + s.getValuation(x).D() * d;
+    Real y_val = s.getValuation(y).R() + s.getValuation(y).D() * d;
+    EXPECT_LT(y_val, 0);
+    EXPECT_LE(x_val, 1);
+    EXPECT_GE(x_val, -5);
+    EXPECT_EQ(x_val, -1 * y_val);
+}
