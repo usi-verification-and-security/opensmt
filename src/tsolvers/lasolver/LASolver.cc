usi-verification-and-security/opensmt

commit 68e3a29f7038e0d06a997f7ce52eb92a19ea66bd

generalized the way how bounds are added to the LASolver so that in LIA version, the bounds can be rounded to respect the integer type of linear terms

src/tsolvers/lasolver/LASolver.cc
@@ -13,6 +13,7 @@ LABoundStore::BoundInfo LASolver::addBound(PTRef leq_tr) {
     const Pterm& leq = logic.getPterm(leq_tr);
     PTRef const_tr = leq[0];
     PTRef sum_tr = leq[1];
+    assert(logic.isNumConst(const_tr) && logic.isLinearTerm(sum_tr));
 
     bool sum_term_is_negated = logic.isNegated(sum_tr);
 
@@ -20,19 +21,18 @@ LABoundStore::BoundInfo LASolver::addBound(PTRef leq_tr) {
     assert(v == laVarMapper.getVarByPTId(logic.getPterm(sum_tr).getId()));
 
     LABoundStore::BoundInfo bi;
-
     LABoundRef br_pos;
     LABoundRef br_neg;
 
     if (sum_term_is_negated) {
         opensmt::Real constr_neg = -logic.getNumConst(const_tr);
-        bi = boundStore.allocBoundPair(v, constr_neg, false);
+        bi = boundStore.allocBoundPair(v, this->getBoundsValue(constr_neg, false));
         br_pos = bi.ub;
         br_neg = bi.lb;
     }
     else {
         const Real& constr = logic.getNumConst(const_tr);
-        bi = boundStore.allocBoundPair(v, constr, true);
+        bi = boundStore.allocBoundPair(v, this->getBoundsValue(constr, true));
         br_pos = bi.lb;
         br_neg = bi.ub;
     }
@@ -484,7 +484,7 @@ void LASolver::initSolver()
 #endif
         auto known_PTRefs = getInformed();
         for(PTRef leq_tr : known_PTRefs) {
-            Pterm& leq_t = logic.getPterm(leq_tr);
+            Pterm const & leq_t = logic.getPterm(leq_tr);
 
             // Terms are of form c <= t where
             //  - c is a constant and

src/tsolvers/lasolver/LASolver.h
@@ -130,6 +130,9 @@ protected:
 
     opensmt::Number getNum(PTRef);
 
+    // Compute the values for an upper bound v ~ c and its negation \neg (v ~ c), where ~ is < if strict and <= if !strict
+    virtual LABoundStore::BoundValuePair getBoundsValue(const Real & c, bool strict) = 0;
+
     LVRef getLAVar_single(PTRef term);                      // Initialize a new LA var if needed, otherwise return the old var
     bool hasVar(PTRef expr);
     LVRef getVarForLeq(PTRef ref)  const  { return laVarMapper.getVarByLeqId(logic.getPterm(ref).getId()); }

src/tsolvers/liasolver/LIASolver.cc
@@ -131,3 +131,21 @@ LIASolver::~LIASolver( )
 }
 
 LIALogic&  LIASolver::getLogic()  { return logic; }
+
+/**
+ * Given an imaginary inequality v ~ c (with ~ is either < or <=), compute the interger bounds on the variable
+ *
+ * @param c Real number representing the upper bound
+ * @param strict inequality is LEQ if false, LT if true
+ * @return The integer values of upper and lower bound corresponding to the inequality
+ */
+LABoundStore::BoundValuePair LIASolver::getBoundsValue(const Real & c, bool strict) {
+    if (strict) {
+        // v < c => UB is ceiling(c-1), LB is ceiling(c)
+        return {Delta((c-1).ceil()), Delta(c.ceil())};
+    }
+    else {
+        // v <= c => UB is floor(c), LB is floor(c+1)
+        return {Delta(c.floor()), Delta((c+1).floor())};
+    }
+}

src/tsolvers/liasolver/LIASolver.h
@@ -67,6 +67,8 @@ protected:
     TRes checkIntegersAndSplit();
     bool isModelInteger (LVRef v) const;
 
+    LABoundStore::BoundValuePair getBoundsValue(const Real & c, bool strict) override;
+
 
 
     opensmt::Integer2 getInt(PTRef r) ;

src/tsolvers/lrasolver/LRASolver.cc
@@ -91,6 +91,24 @@ lbool LRASolver::getPolaritySuggestion(PTRef ptref) const {
     return simplex.getPolaritySuggestion(var, bounds.pos, bounds.neg);
 }
 
+/**
+ * Given an imaginary inequality v ~ c (with ~ is either < or <=), compute the real bounds on the variable
+ *
+ * @param c Real number representing the upper bound
+ * @param strict inequality is LEQ if false, LT if true
+ * @return The real values of upper and lower bound corresponding to the inequality
+ */
+LABoundStore::BoundValuePair LRASolver::getBoundsValue(const Real & c, bool strict) {
+    if (strict) {
+        // v < c => UB is c-\delta, LB is c
+        return { Delta(c, -1), Delta(c) };
+    }
+    else {
+        // v <= c => UB is c, LB is c+\delta
+        return { Delta(c), Delta(c, 1) };
+    }
+}
+
 
 #ifdef PRODUCE_PROOF
 

src/tsolvers/lrasolver/LRASolver.h
@@ -56,8 +56,12 @@ public:
     LRALogic&  getLogic() override;
     lbool getPolaritySuggestion(PTRef) const;
 
+protected:
+    LABoundStore::BoundValuePair getBoundsValue(const Real & c, bool strict) override;
+
 
 #ifdef PRODUCE_PROOF
+public:
     PTRef getInterpolant( const ipartitions_t &, map<PTRef, icolor_t>* );
     PTRef getExperimentalInterpolant( const ipartitions_t & mask , map<PTRef, icolor_t> *labels);
     bool usingStrong() { return config.getLRAInterpolationAlgorithm() == itp_lra_alg_strong; }

src/tsolvers/lasolver/LABounds.h
@@ -123,6 +123,7 @@ class LABoundStore
 {
 public:
     struct BoundInfo { LVRef v; LABoundRef ub; LABoundRef lb; };
+    struct BoundValuePair {Delta upper; Delta lower; };
 private:
     vec<BoundInfo> in_bounds;
     LABoundAllocator ba{1024};
@@ -145,13 +146,15 @@ public:
     LABoundRef getBoundByIdx(LVRef v, int it) const;
     int getBoundListSize(LVRef v) ;
     bool isUnbounded(LVRef v) const;
-    // Construct an upper bound v ~ c and its negation \neg (v ~ c), where ~ is < if strict and <= if !strict
-    BoundInfo allocBoundPair(LVRef v, const opensmt::Real& c, bool strict) {
-        LABoundRef ub = ba.alloc(bound_u, v, strict ? Delta(c, -1) : Delta(c));
-        LABoundRef lb = ba.alloc(bound_l, v, strict ? Delta(c) : Delta(c, 1));
+
+    // Allocates lower and upper bound for LA var with the given values
+    BoundInfo allocBoundPair(LVRef v, BoundValuePair boundPair) {
+        LABoundRef ub = ba.alloc(bound_u, v, std::move(boundPair.upper));
+        LABoundRef lb = ba.alloc(bound_l, v, std::move(boundPair.lower));
         in_bounds.push(BoundInfo{v, ub, lb});
         return in_bounds.last();
     }
+
     LAVarStore const& getVarStore() const { return lvstore; }
 };
 

src/tsolvers/lasolver/LAVarMapper.cc
@@ -44,6 +44,7 @@ void LAVarMapper::addLeqVar(PTRef leq_tr, LVRef v)
     for (int i = leqToLavar.size(); i <= idx; i++) {
         leqToLavar.push(LVRef_Undef);
     }
+    assert(leqToLavar[idx] == LVRef_Undef);
     leqToLavar[idx] = v;
 }
 

commit 30d5170ae5f533c11c495fd25956e16ed24b3423

fix soundness bug in LIA introduced in 68e3a29f, the rounding of bounds should happen only for variables of type Int, but not for complex linear terms, such as x + 1/2y

src/tsolvers/lasolver/LASolver.cc
@@ -26,13 +26,13 @@ LABoundStore::BoundInfo LASolver::addBound(PTRef leq_tr) {
 
     if (sum_term_is_negated) {
         opensmt::Real constr_neg = -logic.getNumConst(const_tr);
-        bi = boundStore.allocBoundPair(v, this->getBoundsValue(constr_neg, false));
+        bi = boundStore.allocBoundPair(v, this->getBoundsValue(v, constr_neg, false));
         br_pos = bi.ub;
         br_neg = bi.lb;
     }
     else {
         const Real& constr = logic.getNumConst(const_tr);
-        bi = boundStore.allocBoundPair(v, this->getBoundsValue(constr, true));
+        bi = boundStore.allocBoundPair(v, this->getBoundsValue(v, constr, true));
         br_pos = bi.lb;
         br_neg = bi.ub;
     }
@@ -734,5 +734,56 @@ LASolver::~LASolver( )
 }
 
 LALogic&  LASolver::getLogic()  { return logic; }
+
+
+/**
+ * Given an inequality v ~ c (with ~ is either < or <=), compute the correct bounds on the variable.
+ * The correct values of the bounds are computed differently, based on whether the value of v must be Int or not.
+ *
+ * @param c Real number representing the upper bound
+ * @param strict inequality is LEQ if false, LT if true
+ * @return The values of upper and lower bound corresponding to the inequality
+ */
+LABoundStore::BoundValuePair LASolver::getBoundsValue(LVRef v, const Real & c, bool strict) {
+    return isIntVar(v) ? getBoundsValueForIntVar(c, strict) : getBoundsValueForRealVar(c, strict);
+}
+
+/**
+ * Given an imaginary inequality v ~ c (with ~ is either < or <=), compute the interger bounds on the variable
+ *
+ * @param c Real number representing the upper bound
+ * @param strict inequality is LEQ if false, LT if true
+ * @return The integer values of upper and lower bound corresponding to the inequality
+ */
+LABoundStore::BoundValuePair LASolver::getBoundsValueForIntVar(const Real & c, bool strict) {
+    if (strict) {
+        // v < c => UB is ceiling(c-1), LB is ceiling(c)
+        return {Delta((c-1).ceil()), Delta(c.ceil())};
+    }
+    else {
+        // v <= c => UB is floor(c), LB is floor(c+1)
+        return {Delta(c.floor()), Delta((c+1).floor())};
+    }
+}
+
+
+/**
+ * Given an imaginary inequality v ~ c (with ~ is either < or <=), compute the real bounds on the variable
+ *
+ * @param c Real number representing the upper bound
+ * @param strict inequality is LEQ if false, LT if true
+ * @return The real values of upper and lower bound corresponding to the inequality
+ */
+LABoundStore::BoundValuePair LASolver::getBoundsValueForRealVar(const Real & c, bool strict) {
+    if (strict) {
+        // v < c => UB is c-\delta, LB is c
+        return { Delta(c, -1), Delta(c) };
+    }
+    else {
+        // v <= c => UB is c, LB is c+\delta
+        return { Delta(c), Delta(c, 1) };
+    }
+}
+
 
 

src/tsolvers/lasolver/LASolver.h
@@ -129,8 +129,11 @@ protected:
 
     opensmt::Number getNum(PTRef);
 
+    virtual bool isIntVar(LVRef v) const { return false; }
     // Compute the values for an upper bound v ~ c and its negation \neg (v ~ c), where ~ is < if strict and <= if !strict
-    virtual LABoundStore::BoundValuePair getBoundsValue(const Real & c, bool strict) = 0;
+    LABoundStore::BoundValuePair getBoundsValue(LVRef v, const Real & c, bool strict);
+    LABoundStore::BoundValuePair getBoundsValueForIntVar(const Real & c, bool strict);
+    LABoundStore::BoundValuePair getBoundsValueForRealVar(const Real & c, bool strict);
 
     LVRef getLAVar_single(PTRef term);                      // Initialize a new LA var if needed, otherwise return the old var
     bool hasVar(PTRef expr);

src/tsolvers/liasolver/LIASolver.cc
@@ -35,6 +35,7 @@ void LIASolver::clearSolver()
 
 void LIASolver::notifyVar(LVRef v)
 {
+    assert(logic.isNumVar(getVarPTRef(v)));
     if (!int_vars_map.has(v)) {
         int_vars_map.insert(v, true);
         int_vars.push(v);
@@ -131,21 +132,3 @@ LIASolver::~LIASolver( )
 }
 
 LIALogic&  LIASolver::getLogic()  { return logic; }
-
-/**
- * Given an imaginary inequality v ~ c (with ~ is either < or <=), compute the interger bounds on the variable
- *
- * @param c Real number representing the upper bound
- * @param strict inequality is LEQ if false, LT if true
- * @return The integer values of upper and lower bound corresponding to the inequality
- */
-LABoundStore::BoundValuePair LIASolver::getBoundsValue(const Real & c, bool strict) {
-    if (strict) {
-        // v < c => UB is ceiling(c-1), LB is ceiling(c)
-        return {Delta((c-1).ceil()), Delta(c.ceil())};
-    }
-    else {
-        // v <= c => UB is floor(c), LB is floor(c+1)
-        return {Delta(c.floor()), Delta((c+1).floor())};
-    }
-}

src/tsolvers/liasolver/LIASolver.h
@@ -67,8 +67,7 @@ protected:
     TRes checkIntegersAndSplit();
     bool isModelInteger (LVRef v) const;
 
-    LABoundStore::BoundValuePair getBoundsValue(const Real & c, bool strict) override;
-
+    virtual bool isIntVar(LVRef v) const override { return int_vars_map.has(v); }
 
 
     opensmt::Integer2 getInt(PTRef r) ;

src/tsolvers/lrasolver/LRASolver.cc
@@ -95,26 +95,6 @@ lbool LRASolver::getPolaritySuggestion(PTRef ptref) const {
     return simplex.getPolaritySuggestion(var, bounds.pos, bounds.neg);
 }
 
-/**
- * Given an imaginary inequality v ~ c (with ~ is either < or <=), compute the real bounds on the variable
- *
- * @param c Real number representing the upper bound
- * @param strict inequality is LEQ if false, LT if true
- * @return The real values of upper and lower bound corresponding to the inequality
- */
-LABoundStore::BoundValuePair LRASolver::getBoundsValue(const Real & c, bool strict) {
-    if (strict) {
-        // v < c => UB is c-\delta, LB is c
-        return { Delta(c, -1), Delta(c) };
-    }
-    else {
-        // v <= c => UB is c, LB is c+\delta
-        return { Delta(c), Delta(c, 1) };
-    }
-}
-
-
-
 
 enum class ItpAlg {
     STRONG, WEAK, FACTOR, EXPERIMENTAL_STRONG, EXPERIMENTAL_WEAK, UNDEF

src/tsolvers/lrasolver/LRASolver.h
@@ -56,9 +56,6 @@ public:
     LRALogic&  getLogic() override;
     lbool getPolaritySuggestion(PTRef) const;
 
-protected:
-    LABoundStore::BoundValuePair getBoundsValue(const Real & c, bool strict) override;
-
 public:
     PTRef getInterpolant( const ipartitions_t &, map<PTRef, icolor_t>* );
     PTRef getDecomposedInterpolant(const ipartitions_t & mask , map<PTRef, icolor_t> *labels);