0944ea84458ec3eeca9c48d9473c2954b11db153
Martin Blicha <blichm@usi.ch>
2020-07-24 10:15:03 +0200

    extended the fix of computing substitutions also to the IDLTheory

--- src/logics/IDLTheory.cc
+++ src/logics/IDLTheory.cc
@@ -13,15 +13,7 @@ bool IDLTheory::simplify(const vec<PFRef>& formulas, int curr)
     PTRef coll_f = getCollateFunction(formulas, curr);
     computeSubstitutions(coll_f, formulas, curr);
     lialogic.simplifyAndSplitEq(pfstore[formulas[curr]].root, pfstore[formulas[curr]].root);
-    vec<Map<PTRef,lbool,PTRefHash>::Pair> units;
-    pfstore[formulas[curr]].units.getKeysAndVals(units);
-    vec<PTRef> substs_vec;
-    for (int i = 0; i < units.size(); i++) {
-        if (units[i].data == l_True) {
-            substs_vec.push(units[i].key);
-        }
-    }
-    PTRef substs_formula = lialogic.mkAnd(substs_vec);
+    PTRef substs_formula = getSubstitutionsFormulaFromUnits(pfstore[formulas[curr]].units);
     lialogic.simplifyAndSplitEq(substs_formula, pfstore[formulas[curr]].substs);
     return true;
 }
