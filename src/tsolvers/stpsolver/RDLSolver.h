64a01fca23a75f4b33191b09cfb8a53b70fac500
Vaclav Lunak <v.lunak@gmail.com>
2020-10-09 17:15:19 +0200

    Created RDLSolver class

---
+++ src/tsolvers/stpsolver/RDLSolver.h
@@ -0,0 +1,30 @@
+#ifndef OPENSMT_RDLSOLVER_H
+#define OPENSMT_RDLSOLVER_H
+
+#include "STPSolver.h"
+#include "Delta.h"
+#include "Converter.h"
+
+class RDLSolver : public STPSolver<Delta> {
+public:
+    RDLSolver(SMTConfig &c, LALogic &l) : STPSolver(c, l) {};
+};
+
+template<>
+Delta Converter<Delta>::getValue(const FastRational &val) {
+    return Delta(val, 0);
+}
+
+template<>
+Delta Converter<Delta>::negate(const Delta &val) {
+    assert(!val.hasDelta());
+    // not(a-b <= c) == (b-a < -c) == (b-a <= -c-delta)
+    return Delta(-val.R(), -1);
+}
+
+template<>
+std::string Converter<Delta>::show(const Delta &val) {
+    return val.printValue();
+}
+
+#endif //OPENSMT_RDLSOLVER_H
