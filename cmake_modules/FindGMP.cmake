4540b910dd3f93ab40122808c2558e6799a21d79
Martin Blicha <martin.blicha@usi.ch>
2020-10-30 16:22:08 +0000

    Ensuring that include directory of GMP library is found and used

--- cmake_modules/FindGMP.cmake
+++ cmake_modules/FindGMP.cmake
@@ -8,9 +8,11 @@ find_library(GMP_LIBRARY NAMES gmp libgmp )
 find_library(GMPXX_LIBRARY NAMES gmpxx libgmpxx )
 mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARY GMPXX_LIBRARY)
 MESSAGE(STATUS "GMP libs: " ${GMP_LIBRARY} " " ${GMPXX_LIBRARY} )
+MESSAGE(STATUS "GMP include: " ${GMP_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARY GMPXX_LIBRARY)
+
 
 if(GMP_FOUND AND NOT TARGET GMP::GMP)
     add_library(GMP::GMP_C UNKNOWN IMPORTED)
@@ -28,6 +30,9 @@ if(GMP_FOUND AND NOT TARGET GMP::GMP)
     set_property(TARGET GMP::GMP APPEND PROPERTY
         INTERFACE_LINK_LIBRARIES GMP::GMP_C GMP::GMP_CXX
         )
+    set_property(TARGET GMP::GMP APPEND PROPERTY
+        INTERFACE_INCLUDE_DIRECTORIES ${GMP_INCLUDE_DIR}
+    )
     # the above command is for compatibillity with old versions of CMAKE, the version below is simpler, but works only from CMake 3.11 
     #target_link_libraries(GMP::GMP INTERFACE GMP::GMP_C GMP::GMP_CXX)
 endif()
