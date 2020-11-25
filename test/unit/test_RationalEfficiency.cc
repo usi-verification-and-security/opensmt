434340b52f84131366c2512bc74d7dc33065272e
Antti Hyvarinen <antti.hyvarinen@gmail.com>
2020-11-25 10:25:44 +0100

    An attempt at fixing a compiler compatibility issue in std::chrono

--- test/unit/test_RationalEfficiency.cc
+++ test/unit/test_RationalEfficiency.cc
@@ -10,6 +10,7 @@
 #include <Sort.h>
 
 using Real = opensmt::Real;
+using namespace std::chrono;
 
 class RationalEfficiencyTest : public ::testing::Test {
     // INT32_MIN
@@ -21,14 +22,16 @@ class RationalEfficiencyTest : public ::testing::Test {
     Real bigmax = rmax*rmax*rmax;
 
     class Timer {
-        using duration = std::chrono::duration<double>;
-        using tp = std::chrono::time_point<std::chrono::steady_clock,duration>;
+        using Clock = high_resolution_clock;
+        using tp = Clock::time_point;
+        using OutDuration = milliseconds;
         tp start;
     public:
-        Timer() : start(std::chrono::high_resolution_clock::now()) {}
+        Timer() : start(Clock::now()) {}
+
         ~Timer() {
-            tp stop = std::chrono::high_resolution_clock::now();
-            duration diff = stop - start;
+            tp stop = Clock::now();
+            auto diff = duration_cast<OutDuration>(stop - start);
             std::cout  << "Time elapsed: " << diff.count() << "s\n";
         }
     };
