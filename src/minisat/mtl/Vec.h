f786bb15e0b2c5f82af98aeb4768cfb50f967eae
Antti <antti.hyvarinen@gmail.com>
2021-01-14 09:05:06 +0100

    Deleted the vec<vec<T>> construct which had undefined behaviour

--- src/minisat/mtl/Vec.h
+++ src/minisat/mtl/Vec.h
@@ -108,71 +108,6 @@ public:
 };
 
 template<class T>
-class vec<vec<T> > {
-    vec<T>*  data;
-    int sz;
-    int cap;
-
-    // Don't allow copying (error prone):
-    vec<vec<T> >&  operator = (vec<vec<T> >& other) = delete;
-                   vec        (vec<vec<T> >& other) = delete;
-
-
-    // Helpers for calculating next capacity:
-    static inline int  imax   (int x, int y) { int mask = (y-x) >> (sizeof(int)*8-1); return (x&mask) + (y&(~mask)); }
-    //static inline void nextCap(int& cap){ cap += ((cap >> 1) + 2) & ~1; }
-    static inline void nextCap(int& cap){ cap += ((cap >> 1) + 2) & ~1; }
-
-public:
-    // Constructors:
-    vec()                            : data(NULL) , sz(0)   , cap(0)    { }
-    explicit vec(int size)           : data(NULL) , sz(0)   , cap(0)    { growTo(size); }
-    vec(int size, const vec<T>& pad) : data(NULL) , sz(0)   , cap(0)    { growTo(size, pad); }
-    vec(const std::initializer_list<std::initializer_list<T> > &iList);
-    vec(vec<vec<T>>&& o)                : data(NULL) , sz(0)   , cap(0)    { std::swap(data, o.data); std::swap(sz, o.sz); std::swap(cap, o.cap); }
-    vec<vec<T>>& operator = (vec<vec<T>>&& o) { std::swap(data, o.data); std::swap(sz, o.sz); std::swap(cap, o.cap); return *this; }
-    ~vec()                                                          { clear(true); }
-
-    // Pointer to first element:
-    operator vec<T>*  (void)           { return data; }
-
-    // Size operations:
-    int      size     (void) const     { return sz; }
-    uint32_t size_    (void) const     { assert(sz >= 0); return (uint32_t) sz; }
-    void     shrink   (int nelems)     { assert(nelems <= sz); for (int i = 0; i < nelems; i++) sz--, data[sz].~vec<T>(); }
-    void     shrink_  (int nelems)     { assert(nelems <= sz); sz -= nelems; }
-    int      capacity (void) const     { return cap; }
-    void     capacity (int min_cap);
-    void     growTo   (int size);
-    void     growTo   (int size, const vec<T>& pad);
-    void     clear    (bool dealloc = false);
-    void     reset    ();
-
-    // Stack interface:
-    void     push  (void)               { if (sz == cap) capacity(sz+1); new (&data[sz]) vec<T>(); sz++; }
-    void     push  (vec<T>&& elem)      { if (sz == cap) capacity(sz+1); new (&data[sz]) vec<T>(); data[sz++] = std::move(elem); }
-    void     push  (const vec<T>& elem) { if (sz == cap) capacity(sz+1); data[sz++] = elem; }
-
-    void     push_ (const T& elem)      { assert(sz < cap); data[sz++] = elem; }
-    void     pop   (void)               { assert(sz > 0); sz--, data[sz].~vec<T>(); }
-    // NOTE: it seems possible that overflow can happen in the 'sz+1' expression of 'push()', but
-    // in fact it can not since it requires that 'cap' is equal to INT_MAX. This in turn can not
-    // happen given the way capacities are calculated (below). Essentially, all capacities are
-    // even, but INT_MAX is odd.
-
-    const vec<T>& last  (void) const        { return data[sz-1]; }
-    vec<T>&       last  (void)              { return data[sz-1]; }
-
-    // Vector interface:
-    const vec<T>& operator [] (int index) const { return data[index]; }
-    vec<T>&       operator [] (int index)       { return data[index]; }
-
-    // Duplicatation (preferred instead):
-    void copyTo(vec<vec<T> >& copy) const { copy.clear(); copy.growTo(sz); for (int i = 0; i < sz; i++) data[i].copyTo(copy[i]); }
-    void moveTo(vec<vec<T> >& dest) { dest.clear(true); dest.data = data; dest.sz = sz; dest.cap = cap; data = NULL; sz = 0; cap = 0; }
-};
-
-template<class T>
 vec<T>::vec(const std::initializer_list<T> &iList) : data(NULL), sz(0), cap(0) {
     this->growTo(iList.size());
     int i = 0;
@@ -191,15 +126,6 @@ void vec<T>::capacity(int min_cap) {
  }
 
 template<class T>
-void vec<vec<T> >::capacity(int min_cap) {
-    if (cap >= min_cap) return;
-    int add = imax((min_cap - cap + 1) & ~1, ((cap >> 1) + 2) & ~1);   // NOTE: grow by approximately 3/2
-    if (add > INT_MAX - cap || (((data = (vec<T>*)::realloc(data, (cap += add) * sizeof(vec<T>))) == NULL) && errno == ENOMEM))
-        throw OutOfMemoryException();
-}
-
-
-template<class T>
 void vec<T>::growTo(int size, const T& pad) {
     if (sz >= size) return;
     capacity(size);
@@ -207,27 +133,11 @@ void vec<T>::growTo(int size, const T& pad) {
     sz = size; }
 
 template<class T>
-void vec<vec<T> >::growTo(int size, const vec<T>& pad) {
-    if (sz >= size) return;
-    capacity(size);
-    for (int i = sz; i < size; i++) data[i] = pad;
-    sz = size; }
-
-
-template<class T>
 void vec<T>::growTo(int size) {
     if (sz >= size) return;
     capacity(size);
     for (int i = sz; i < size; i++) new (&data[i]) T();
     sz = size; }
-
-template<class T>
-void vec<vec<T> >::growTo(int size) {
-    if (sz >= size) return;
-    capacity(size);
-    for (int i = sz; i < size; i++) new (&data[i]) vec<T>();
-    sz = size; }
-
 
 template<class T>
 void vec<T>::clear(bool dealloc) {
@@ -242,21 +152,6 @@ void vec<T>::reset() {
     sz = 0;
     cap = 0;
 }
-
-template<class T>
-void vec<vec<T> >::clear(bool dealloc) {
-    if (data != NULL){
-        for (int i = 0; i < sz; i++) data[i].~vec<T>();
-        sz = 0;
-        if (dealloc) free(data), data = NULL, cap = 0; } }
-
-template<class T>
-void vec<vec<T> >::reset() {
-    data = 0;
-    sz = 0;
-    cap = 0;
-}
-
 
 //=================================================================================================
 
