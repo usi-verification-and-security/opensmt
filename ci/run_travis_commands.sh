80a31a312b9b56ec492e169d1479ca60a2ba9a7c
Antti Hyvärinen <antti.hyvarinen@gmail.com>
2021-01-20 17:16:46 +0100

    Add support for CircleCI (#214)
    Add support for CircleCI
    * A default Debian system from circleci for building and testing with the system default g++ and clang++
    * A starexec-like centos7 docker image build job for circleci

--- ci/run_travis_commands.sh
+++ ci/run_travis_commands.sh
@@ -3,8 +3,8 @@
 set -ev
 cd build
 cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_CXX_FLAGS="$FLAGS" -DUSE_READLINE:BOOL=${USE_READLINE} -DCMAKE_INSTALL_PREFIX=${INSTALL} -DPACKAGE_BENCHMARKS=${PACKAGE_BENCHMARKS} ..
-make
-ctest
+make -j4
+make test
 make install
 if [[ ${CMAKE_BUILD_TYPE} == Debug ]]; then
     cd ../regression && ./run-test-notiming.sh ../build/opensmt;
@@ -12,5 +12,19 @@ if [[ ${CMAKE_BUILD_TYPE} == Debug ]]; then
     cd ../regression_splitting && ./bin/run-tests.sh ../build/opensmt;
 fi
 
-cd ../examples && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DOpenSMT_DIR=${INSTALL} ..
-make
+cd ../examples && rm -rf build && mkdir -p build && cd build
+if [[ ${CMAKE_BUILD_TYPE} == Debug ]]; then
+    cmake \
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+        -DOpenSMT_DIR=${INSTALL} \
+        -DCMAKE_EXE_LINKER_FLAGS=-fsanitize=address,undefined \
+        ..
+else
+    cmake \
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+        -DOpenSMT_DIR=${INSTALL} \
+        ..
+fi
+
+make -j4
+
