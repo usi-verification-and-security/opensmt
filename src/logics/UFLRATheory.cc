e8c8636efef1bbf883f9d51872d99ca8b83ce036
Antti <antti.hyvarinen@gmail.com>
2021-03-10 17:53:39 +0100

    A better implementation for UFLRATheory.  This is untested, since we don't yet have theory combination

--- src/logics/UFLRATheory.cc
+++ src/logics/UFLRATheory.cc
@@ -1,15 +1,22 @@
 #include "UFLRATheory.h"
+#include "TreeOps.h"
 #include "ArithmeticEqualityRewriter.h"
 
 bool UFLRATheory::simplify(const vec<PFRef>& formulas, PartitionManager &pmanager, int curr)
 {
     auto & currentFrame = pfstore[formulas[curr]];
     // Take care of UF simplifications as well
+    ArithmeticEqualityRewriter rewriter(lralogic);
     if (this->keepPartitions()) {
-        currentFrame.root = getLogic().mkAnd(currentFrame.formulas);
-        auto arr = ArithmeticEqualityRewriter(lralogic);
-        currentFrame.root = arr.rewrite(currentFrame.root);
-        notOkToPartition = arr.getAndClearNotOkToPartition();
+        vec<PTRef> & flas = currentFrame.formulas;
+        ArithmeticEqualityRewriter rewriter(lralogic);
+        for (PTRef & fla : flas) {
+            PTRef old = fla;
+            fla = rewriter.rewrite(old);
+            pmanager.transferPartitionMembership(old, fla);
+        }
+        notOkToPartition = rewriter.getAndClearNotOkToPartition();
+        currentFrame.root = getLogic().mkAnd(flas);
     } else {
         PTRef coll_f = getCollateFunction(formulas, curr);
         auto subs_res = computeSubstitutions(coll_f);
@@ -17,5 +24,6 @@ bool UFLRATheory::simplify(const vec<PFRef>& formulas, PartitionManager &pmanage
         getTSolverHandler().setSubstitutions(subs_res.usedSubstitution);
         currentFrame.root = ArithmeticEqualityRewriter(lralogic).rewrite(finalFla);
     }
+    AppearsInUfVisitor(getLogic()).visit(currentFrame.root);
     return true;
 }
