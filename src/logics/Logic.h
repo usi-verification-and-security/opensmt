538f8d21bdaa8093f301386326a5c4570032746f
Antti <antti.hyvarinen@gmail.com>
2021-03-10 17:52:16 +0100

    Using updated includes

--- src/logics/Logic.h
+++ src/logics/Logic.h
@@ -31,6 +31,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "SStore.h"
 #include "CgTypes.h"
 #include "LogicFactory.h"
+#include <cassert>
+#include <cstring>
+#include <cstdlib>
 
 
 class SStore;
