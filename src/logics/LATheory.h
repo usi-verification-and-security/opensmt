db1c477ee6ad4f7c474b8ae8d4ca42e0837e83c6
Antti <antti.hyvarinen@gmail.com>
2021-03-11 09:53:33 +0100

    Refactoring and fixes for rewriter use in simplifiers

--- src/logics/LATheory.h
+++ src/logics/LATheory.h
@@ -37,25 +37,23 @@ template<typename LinAlgLogic, typename LinAlgTSHandler>
 bool LATheory<LinAlgLogic,LinAlgTSHandler>::simplify(const vec<PFRef>& formulas, PartitionManager& pmanager, int curr)
 {
     auto & currentFrame = pfstore[formulas[curr]];
+    ArithmeticEqualityRewriter rewriter(lalogic);
     if (this->keepPartitions()) {
         vec<PTRef> & flas = currentFrame.formulas;
-        ArithmeticEqualityRewriter rewriter(lalogic);
         for (PTRef & fla : flas) {
             PTRef old = fla;
             fla = rewriter.rewrite(old);
             pmanager.transferPartitionMembership(old, fla);
         }
-        notOkToPartition = rewriter.getAndClearNotOkToPartition();
         currentFrame.root = getLogic().mkAnd(flas);
     } else {
         PTRef coll_f = getCollateFunction(formulas, curr);
         auto subs_res = computeSubstitutions(coll_f);
         PTRef finalFla = flaFromSubstitutionResult(subs_res);
         getTSolverHandler().setSubstitutions(subs_res.usedSubstitution);
-        ArithmeticEqualityRewriter rewriter(lalogic);
         currentFrame.root = rewriter.rewrite(finalFla);
-        notOkToPartition = rewriter.getAndClearNotOkToPartition();
     }
+    notOkToPartition = rewriter.getAndClearNotOkToPartition();
     return true;
 }
 
--- src/logics/UFLRATheory.cc
+++ src/logics/UFLRATheory.cc
@@ -9,21 +9,20 @@ bool UFLRATheory::simplify(const vec<PFRef>& formulas, PartitionManager &pmanage
     ArithmeticEqualityRewriter rewriter(lralogic);
     if (this->keepPartitions()) {
         vec<PTRef> & flas = currentFrame.formulas;
-        ArithmeticEqualityRewriter rewriter(lralogic);
         for (PTRef & fla : flas) {
             PTRef old = fla;
             fla = rewriter.rewrite(old);
             pmanager.transferPartitionMembership(old, fla);
         }
-        notOkToPartition = rewriter.getAndClearNotOkToPartition();
         currentFrame.root = getLogic().mkAnd(flas);
     } else {
         PTRef coll_f = getCollateFunction(formulas, curr);
         auto subs_res = computeSubstitutions(coll_f);
         PTRef finalFla = flaFromSubstitutionResult(subs_res);
         getTSolverHandler().setSubstitutions(subs_res.usedSubstitution);
-        currentFrame.root = ArithmeticEqualityRewriter(lralogic).rewrite(finalFla);
+        currentFrame.root = rewriter.rewrite(finalFla);
     }
+    notOkToPartition = rewriter.getAndClearNotOkToPartition();
     AppearsInUfVisitor(getLogic()).visit(currentFrame.root);
     return true;
 }
