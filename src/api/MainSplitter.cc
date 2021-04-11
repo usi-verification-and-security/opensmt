3d2cb6368cdecf3ca9d2f6fc923c2a61c5072e9f
usi <usi@usis-MacBook-Pro.local>
2021-04-11 18:31:22 +0200

    Cast to the its corresponding splitter in writeSolverSplits_smtlib2

--- src/api/MainSplitter.cc
+++ src/api/MainSplitter.cc
@@ -10,6 +10,7 @@
 bool MainSplitter::writeSolverSplits_smtlib2(const char* file, char** msg) const
 {
     std::vector<SplitData>& splits =(config.sat_split_type()==spt_scatter) ? static_cast<ScatterSplitter&>(ts.solver).splits
+
                                                                            : static_cast<LookaheadSplitter&>(ts.solver).splits;
     if(config.sat_split_type() == spt_scatter)
         std::cout <<"Number of splits created by scatter splitter: "<< splits.size() << "\n";
