ed46ec16088928a1b9fc8455ddd5505c60de29f7
Antti <antti.hyvarinen@gmail.com>
2021-05-05 18:31:52 +0200

    Implementing a tree-based store for UF function model values

--- src/models/ModelBuilder.cc
+++ src/models/ModelBuilder.cc
@@ -5,15 +5,12 @@
 #include "ModelBuilder.h"
 #include <unordered_set>
 
-int ModelBuilder::ValuationNode::counter = 0;
-
 std::unique_ptr<Model> ModelBuilder::build() {
     std::unordered_map<SymRef,TemplateFunction,SymRefHash> builtDefinitions;
     for (auto & symbolSigVal : definitions) {
         const ValuationNode * valuationNode = symbolSigVal.second.second;
         SRef sr = logic.getSortRef(symbolSigVal.first);
         PTRef body = valuationTreeToFunctionBody(valuationNode, sr);
-        // todo: how to free the valuationNode?
         TemplateFunction templateFun(std::move(symbolSigVal.second.first), body);
         builtDefinitions[symbolSigVal.first] = std::move(templateFun);
     }
@@ -59,40 +56,83 @@ void ModelBuilder::addToTheoryFunction(SymRef sr, vec<PTRef> vals, PTRef val)
     assert(logic.getSortRef(val) == logic.getSortRef(sr));
     signatureAndValuation.second = addToValuationTree(valuation, val, signatureAndValuation.second);
 }
+
+ModelBuilder::ValuationNode * ModelBuilder::addToValuationTree(const vec<opensmt::pair<PTRef,PTRef>> & valuation, PTRef value, ValuationNode * root) {
+    assert(valuation.size() >= 1);
+
+    if (root == nullptr) {
+        root = valuationNodeFactory.getValuationNode(valuation[0].first, PTRef_Undef);
+    }
+
+    ValuationNode * current = root;
+
+    for (int i = 0; i < valuation.size(); i++) {
+        auto & varVal = valuation[i];
+        PTRef val = varVal.second; // The value of my parent's var
+
+        assert(current != nullptr);
+
+        bool foundChild = false;
+        for (auto child: *current) {
+            if (child->getValue() == val) {
+                current = child;
+                foundChild = true;
+                break;
+            }
+        }
+        if (foundChild) {
+            continue;
+        } else {
+            // No child found with this value.  Need to create a new child.  If this will be a leaf
+            // (i.e., it is the last value in a valuation) the var will by convention be set to the
+            // value of this valuation
+            current = current->addChild(valuationNodeFactory.getValuationNode(i == valuation.size()-1 ? value : valuation[i+1].first, val));
+        }
     }
     return root;
 }
 
-PTRef ModelBuilder::valuationTreeToFunctionBody(const ValuationNode *root) {
-    using QPair = opensmt::pair<const ValuationNode*,std::size_t>;
+// Copied from TermVisitor.  Maybe it'd be possible to template TermVisitor to handle also ValuationNodes
+PTRef ModelBuilder::valuationTreeToFunctionBody(const ValuationNode *root, SRef returnSort) {
+    struct QPair {
+        const ValuationNode * node;
+        std::size_t nextChild;
+        QPair(const ValuationNode* n) : node(n), nextChild(0) {}
+    };
+
     std::unordered_map<const ValuationNode*,PTRef,ValuationNodeHash> nodeToFormula;
+
+    vec<char> done(valuationNodeFactory.numNodes(), 0);
+
     vec<QPair> queue;
-    queue.push(QPair{root, 0});
+    queue.push(QPair{root});
     while (queue.size() != 0) {
         auto & el = queue.last();
-        auto * node = el.first;
-        std::size_t & processedUntil = el.second;
-        if (processedUntil < node->children.size()) {
-            const ValuationNode *child = (node->children[processedUntil]);
-            if (nodeToFormula.find(child) == nodeToFormula.end()) {
-                queue.push({QPair{child, 0}});
-                nodeToFormula.insert({child,PTRef_Undef});
+        auto * node = el.node;
+        assert(not done[node->getId()]);
+        if (el.nextChild < node->nChildren()) {
+            const ValuationNode *child = (*node)[el.nextChild];
+            el.nextChild ++;
+            if (not done[child->getId()]) {
+                queue.push({QPair{child}});
             }
-            processedUntil += 1;
             continue;
         }
         // All children of the node are processed
-        if (node->children.empty()) {
+        assert(done[node->getId()] == 0);
+        if (not node->hasChildren()) {
             // Base case: the node is a leaf value node.
-            PTRef &tr = nodeToFormula.at(node);
-            tr = node->value;
+            nodeToFormula[node] = node->getVar();
+            assert(logic.getSortRef(node->getVar()) == returnSort);
         } else {
-            PTRef nodeFormula = logic.getDefaultValuePTRef(node->var);
-            for (int i = node->children.size()-1; i >= 0; i--) {
-                nodeFormula = logic.mkIte(logic.mkEq(node->var, node->children[i]->value), nodeToFormula.at(node->children[i]), nodeFormula);
+            PTRef nodeFormula = logic.getDefaultValuePTRef(returnSort);
+            for (int i = node->nChildren()-1; i >= 0; i--) {
+                nodeFormula = logic.mkIte(logic.mkEq(node->getVar(), (*node)[i]->getValue()), nodeToFormula.at((*node)[i]), nodeFormula);
             }
             nodeToFormula[node] = nodeFormula;
         }
+        done[node->getId()] = 1;
+        queue.pop();
     }
     return nodeToFormula[root];
 }
