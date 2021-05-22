fa00169e60833dce0d3b74f4b15486228ed6d5db
Antti <antti.hyvarinen@gmail.com>
2021-05-22 17:33:03 +0200

    Remove duplicate code

--- src/models/ModelBuilder.cc
+++ src/models/ModelBuilder.cc
@@ -24,19 +24,5 @@ void ModelBuilder::processSubstitutions(MapWithKeys<PTRef,PtAsgn,PTRefHash> cons
             assignCopy.insert(std::make_pair(key, val));
         }
     }
-    for (auto key : copy.getKeys()) {
-        if (logic.isIte(key)) {
-            // We store only values of reals variables
-            continue;
-        }
-        assert(logic.isVar(key));
-        PtAsgn target = copy[key];
-        if (target.sgn == l_True) {
-            PTRef mappedTerm = target.tr;
-            PTRef val = model->evaluate(mappedTerm);
-            assert(logic.isConstant(val));
-            assignCopy.insert(std::make_pair(key, val));
-        }
-    }
     this->assignment = std::move(assignCopy);
 }
