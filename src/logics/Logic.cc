usi-verification-and-security/opensmt

commit c5698fbf2bae4e1e07a78311e309c34d9eb877bb

Model fix (#298)
* UF Models: fix a regression that caused sort information to disappear.

* CI: fix a regression test so that `get-value` reports sorts in UF when they are not known

Co-authored-by: Martin Blicha <blichm@usi.ch>

src/logics/Logic.cc
@@ -340,46 +340,36 @@ Logic::pp(PTRef tr) const
 char*
 Logic::printTerm_(PTRef tr, bool ext, bool safe) const
 {
-    char* out;
+    std::stringstream ss;
 
     const Pterm& t = getPterm(tr);
     SymRef sr = t.symb();
-    char* name_escaped = printSym(sr);
+    char* name_escaped_c = printSym(sr);
+
+    std::string name_escaped = name_escaped_c;
+    free(name_escaped_c);
 
     if (t.size() == 0) {
-        int res = ext ? asprintf(&out, "%s <%d>", name_escaped, tr.x)
-                : asprintf(&out, "%s", name_escaped);
-        assert(res >= 0); (void)res;
-        free(name_escaped);
-        return out;
-    }
-
-    // Here we know that t.size() > 0
-
-    char* old;
-    int chars_written = asprintf(&out, "(%s ", name_escaped);
-    assert(chars_written >= 0);
-    free(name_escaped);
-
-    for (int i = 0; i < t.size(); i++) {
-        old = out;
-        char * current_term = printTerm_(t[i], ext, safe);
-        chars_written = asprintf(&out, "%s%s", old, current_term);
-        assert(chars_written >= 0);
-        ::free(old);
-        ::free(current_term);
-        if (i < t.size()-1) {
-            old = out;
-            chars_written = asprintf(&out, "%s ", old);
-            assert(chars_written >= 0);
-            ::free(old);
+        std::string ext_string = ext ? " <" + std::to_string(tr.x) + ">" : "";
+        if (isKnownToUser(sr)) {
+            ss << name_escaped << (ext ? ext_string : "");
+        } else {
+            ss << "(as " << name_escaped << ext_string << " " << getSortName(getSortRef(sr)) << ")";
         }
+        return strdup(ss.str().data());
+    } else {
+        // Here we know that t.size() > 0
+        ss << "(" << name_escaped;
+        bool first = true;
+        for (auto arg : t) {
+            char *current_term_p = printTerm_(arg, ext, safe);
+            ss << (first ? "" : " ") << current_term_p;
+            free(current_term_p);
+            first = false;
+        }
+        ss << ")" << (ext ? " <" + std::to_string(tr.x) + ">" : "");
     }
-    old = out;
-    chars_written = ext ? asprintf(&out, "%s) <%d>", old, tr.x) : asprintf(&out, "%s)", old);
-    assert(chars_written >= 0); (void)chars_written;
-    ::free(old);
-    return out;
+    return strdup(ss.str().data());
 }
 
 bool Logic::isTheoryTerm(PTRef ptr) const {

regression/QF_UF/get-value.smt2.expected.out
@@ -1,2 +1,2 @@
 sat
-((f @2)(g @2)(h @3)(true true)(false false))
+((f (as @2 U))(g (as @2 U))(h (as @3 U))(true true)(false false))

commit 2d24711c0aec0b662862d75e72302a4b91ae4254

Fix: a bug introduced in c5698fbf resulted in no space being printed between a term and its first argument

src/logics/Logic.cc
@@ -360,12 +360,10 @@ Logic::printTerm_(PTRef tr, bool ext, bool safe) const
     } else {
         // Here we know that t.size() > 0
         ss << "(" << name_escaped;
-        bool first = true;
         for (auto arg : t) {
             char *current_term_p = printTerm_(arg, ext, safe);
-            ss << (first ? "" : " ") << current_term_p;
+            ss << " " << current_term_p;
             free(current_term_p);
-            first = false;
         }
         ss << ")" << (ext ? " <" + std::to_string(tr.x) + ">" : "");
     }