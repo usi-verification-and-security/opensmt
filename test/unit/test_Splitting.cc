75dfb62c5564a9c7d8824d4a01a1982fa0c995b3
Antti <antti.hyvarinen@gmail.com>
2021-09-30 18:10:53 +0200

    CI: A unit test for printing terms (used in splitting code)

---
+++ test/unit/test_Splitting.cc
@@ -0,0 +1,26 @@
+//
+// Created by Antti on 30.09.21.
+//
+
+#include <gtest/gtest.h>
+#include <Logic.h>
+
+class SplitTest : public ::testing::Test {
+protected:
+    SplitTest() : logic{} {}
+    Logic logic;
+};
+
+TEST_F(SplitTest, test_TermPrinting) {
+    char * msg;
+    SRef U = logic.declareSort("U", &msg);
+    SymRef sr = logic.declareFun("f", U, {U, U}, &msg, false);
+    PTRef a = logic.mkVar(U, "a");
+    PTRef f_a = logic.mkUninterpFun(sr, {a, a});
+    char * str_p = logic.printTerm(f_a);
+    std::string str(str_p);
+    std::string reference = "(f a a)";
+    free(str_p);
+    std::cout << str << endl;
+    ASSERT_EQ(str.compare(reference), 0);
+}
