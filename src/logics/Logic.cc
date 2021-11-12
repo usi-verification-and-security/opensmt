b07020ce80523c2ae3d1cc711ac95fa11eb10d17
Antti <antti.hyvarinen@gmail.com>
2021-11-12 15:06:48 +0100

    [logic] Fix argument numbers for Boolean Ite

--- src/logics/Logic.cc
+++ src/logics/Logic.cc
@@ -87,7 +87,7 @@ Logic::Logic() :
     , sym_EQ(declareFun_Commutative_NoScoping_Chainable(tk_equals, sort_BOOL, {sort_BOOL, sort_BOOL}))
     , sym_IMPLIES(declareFun_NoScoping_RightAssoc(tk_implies, sort_BOOL, {sort_BOOL, sort_BOOL}))
     , sym_DISTINCT(declareFun_Commutative_NoScoping_Pairwise(tk_distinct, sort_BOOL, {sort_BOOL, sort_BOOL}))
-    , sym_ITE(declareFun_NoScoping_LeftAssoc(tk_ite, sort_BOOL, {sort_BOOL, sort_BOOL}))
+    , sym_ITE(declareFun_NoScoping(tk_ite, sort_BOOL, {sort_BOOL, sort_BOOL, sort_BOOL}))
     , use_extended_signature(false)
 {
     ites.insert(sym_ITE, true);
