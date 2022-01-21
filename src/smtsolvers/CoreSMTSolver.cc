f687d06a1f10bbd15409fa8d90c65a4ecea3b207
Konstantin <BritikovKI@yandex.ru>
2022-01-21 13:50:06 +0100

    logging

--- src/smtsolvers/CoreSMTSolver.cc
+++ src/smtsolvers/CoreSMTSolver.cc
@@ -1174,7 +1174,7 @@ CRef CoreSMTSolver::propagate()
 
     while (qhead < trail.size())
     {
-//        props++;
+        props++;
         Lit            p   = trail[qhead++];     // 'p' is enqueued fact to propagate.
         vec<Watcher>&  ws  = watches[p];
         Watcher        *i, *j, *end;
