66b3daf66c4abf7c8edd80a8326c43801beaefa5
Antti Hyvärinen <antti.hyvarinen@gmail.com>
2022-02-04 15:10:45 +0100

    Lookahead: fix memory leak and remove code duplication

--- src/smtsolvers/LookaheadSplitter.h
+++ src/smtsolvers/LookaheadSplitter.h
@@ -14,26 +14,45 @@ protected:
     {
     public:
         // The children
-        SplitData* sd;
-        LASplitNode() : sd(nullptr) {}
-        ~LASplitNode() override { delete sd; }
+        std::unique_ptr<SplitData> sd;
+
+        LASplitNode * getParent() override { return (LASplitNode*)p; }
+
         void print_local() override {
             LANode::print_local();
             for (int i = 0; i < d; i++)
                 dprintf(STDERR_FILENO, " ");
             dprintf(STDERR_FILENO, "%s\n", sd == nullptr ? "no instance" : "has instance" );
         }
-        LASplitNode* getC1() { return (LASplitNode*) c1; }
-        LASplitNode* getC2() { return (LASplitNode*) c2; }
+        LASplitNode const * getC1() const { return (LASplitNode*) c1.get(); }
+        LASplitNode const * getC2() const { return (LASplitNode*) c2.get(); }
     };
-    void copySplits(LASplitNode *root);
+    void copySplits(LASplitNode const & root);
 
+    bool createSplitLookahead(LASplitNode &);
+
+    struct SplitBuildConfig {
+    private:
+        LookaheadSplitter & splitter;
     public:
+        bool stopCondition(LASplitNode & n, int num_split) {
+            int maxDepth = getLog2Ceil(num_split);
+            if (n.d == maxDepth) {
+#ifdef LADEBUG
+                printf("Producing a split:\n");;
+            printTrace();
+#endif
+                splitter.createSplitLookahead(n);
+                return true;
+            }
+            return false;
+        }
+        LALoopRes exitState() const { return LALoopRes::unknown_final; }
+        SplitBuildConfig(LookaheadSplitter & splitter_) : splitter(splitter_) {}
+    };
+
+public:
     LookaheadSplitter(SMTConfig& c, THandler& thandler) : LookaheadSMTSolver(c, thandler) {}
-    bool     createSplitLookahead(LASplitNode*);
-
-
 };
-
 
 #endif //OPENSMT_LOOKAHEADSPLITTER_H
