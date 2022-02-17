62abf2845ae668f9c60d2e943a7c15584629870f
BritikovKI <BritikovKI@yandex.ru>
2022-02-17 16:50:23 +0100

    new upds

--- src/smtsolvers/Proof.cc
+++ src/smtsolvers/Proof.cc
@@ -51,11 +51,10 @@ void Proof::beginChain( CRef c )
 
     assert( c != CRef_Undef );
     assert( !hasOpenChain() );
-//    std::cout<<"Has begun chain\n";
-//    if(c == 1509 || c == 4936){
-//        std::cout<<"Hi!";
-//    }
-//    printf("BegUN chain %d\n", c);
+    if(c == 281){
+        std::cout<<"Hi!";
+    }
+    printf("BegUN chain %d\n", c);
     begun = true;
     assert(current_chain.isEmpty());
     assert( clause_to_proof_der.find( c ) != clause_to_proof_der.end( ) );
@@ -82,7 +81,7 @@ void Proof::addResolutionStep(CRef c, Var p)
 void Proof::endChain( CRef conclusion )
 {
   assert(hasOpenChain());
-//  std::cout<<"Has ended chain: " << conclusion << "\n";
+  std::cout<<"Has ended chain: " << conclusion << "\n";
 //  if(conclusion == 1509){
 //      printf("Lol\n");
 //  }
