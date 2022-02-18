ddc06ebe38e15cd3a40277bd6f2d52f1cdd017a2
Antti Hyvärinen <antti.hyvarinen@gmail.com>
2022-02-18 17:01:40 +0100

    TermPrinter: Some unit tests

---
+++ test/unit/test_TermPrinter.cc
@@ -0,0 +1,66 @@
+//
+// Created by Antti on 18.02.22.
+//
+
+#include <gtest/gtest.h>
+#include <Logic.h>
+#include <TermPrinter.h>
+
+class TermPrinterTest : public ::testing::Test {
+public:
+    Logic logic;
+    TermPrinterTest() : logic{opensmt::Logic_t::QF_UF} {}
+
+    PTRef buildExampleFormula1() {
+        SRef sort = logic.declareUninterpretedSort("U");
+        PTRef x = logic.mkVar(sort, "x");
+        PTRef y = logic.mkVar(sort, "y");
+        SymRef g_s = logic.declareFun("g", sort, {sort, sort});
+        SymRef h_s = logic.declareFun("h", sort, {sort});
+        SymRef f_s = logic.declareFun("f", sort, {sort, sort, sort});
+        PTRef g = logic.mkUninterpFun(g_s, {x, y});
+        PTRef h = logic.mkUninterpFun(h_s, {y});
+        PTRef f = logic.mkUninterpFun(f_s, {g, g, h});
+        return logic.mkEq(f, h);
+    }
+
+    PTRef buildExampleFormula2() {
+        vec<PTRef> shared;
+        SRef sort = logic.declareUninterpretedSort("U");
+        PTRef x = logic.mkVar(sort, "x");
+        PTRef y = logic.mkVar(sort, "y");
+        SymRef g_s = logic.declareFun("g", sort, {sort, sort});
+        SymRef h_s = logic.declareFun("h", sort, {sort});
+        SymRef f_s = logic.declareFun("f", sort, {sort, sort, sort});
+        PTRef g = logic.mkUninterpFun(g_s, {x, y});
+        PTRef h = logic.mkUninterpFun(h_s, {g});
+        PTRef f = logic.mkUninterpFun(f_s, {g, g, h});
+        return logic.mkEq(f, h);
+    }
+
+    PTRef buildExampleFormula3() {
+        vec<PTRef> shared;
+        SRef sort = logic.declareUninterpretedSort("U");
+        PTRef x = logic.mkVar(sort, "x");
+        PTRef y = logic.mkVar(sort, "y");
+        SymRef g_s = logic.declareFun("g", sort, {sort, sort});
+        SymRef h_s = logic.declareFun("h", sort, {sort, sort});
+        SymRef f_s = logic.declareFun("f", sort, {sort, sort, sort});
+        SymRef r_s = logic.declareFun("r", sort, {sort});
+        PTRef g = logic.mkUninterpFun(g_s, {x, y});
+        PTRef r = logic.mkUninterpFun(r_s, {y});
+        PTRef h = logic.mkUninterpFun(h_s, {r, r});
+        PTRef f = logic.mkUninterpFun(f_s, {g, g, h});
+        return logic.mkEq(f, h);
+    }
+};
+
+TEST_F(TermPrinterTest, test_TermPrinting) {
+    PTRef ex1 = buildExampleFormula1();
+    PTRef ex2 = buildExampleFormula2();
+    PTRef ex3 = buildExampleFormula3();
+    for (PTRef ex : {ex1, ex2, ex3}) {
+        std::cout << TermPrinter(logic).print(ex) << std::endl;
+        ASSERT_EQ (TermPrinter(logic).print(ex), logic.pp(ex));
+    }
+}
