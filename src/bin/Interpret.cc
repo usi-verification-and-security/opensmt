562efb58138d558ed3fceb0dbd334b5ac3daf143
Antti Hyvärinen <antti.hyvarinen@gmail.com>
2022-02-18 17:03:17 +0100

    Interpret: Use the new TermPrinter for printing models and interpolants

--- src/bin/Interpret.cc
+++ src/bin/Interpret.cc
@@ -31,6 +31,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "MainSplitter.h"
 #include "LogicFactory.h"
 #include "Substitutor.h"
+#include "TermPrinter.h"
 
 #include <string>
 #include <sstream>
@@ -816,7 +817,7 @@ std::string Interpret::printDefinitionSmtlib(const TemplateFunction & templateFu
         ss << "(" << logic->protectName(logic->getSymRef(args[i])) << " " << sortString << ")" << (i == args.size()-1 ? "" : " ");
     }
     ss << ")" << " " << logic->printSort(templateFun.getRetSort()) << "\n";
-    ss << "    " << logic->pp(templateFun.getBody()) << ")\n";
+    ss << "    " << TermPrinter(*logic).print(templateFun.getBody()) << ")\n";
     return ss.str();
 }
 
@@ -1308,7 +1309,7 @@ void Interpret::getInterpolants(const ASTNode& n)
     }
 
     for (int j = 0; j < itps.size(); j++) {
-        auto itp = logic->pp(itps[j]);
+        auto itp = TermPrinter(*logic).print(itps[j]);
         notify_formatted(false, "%s%s%s",
                          (j == 0 ? "(" : " "), itp.c_str(), (j == itps.size() - 1 ? ")" : ""));
     }
