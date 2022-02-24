e37a32e1d9681c627743ad13cb60adca0f86ac8e
Martin Blicha <martin.blicha@usi.ch>
2022-02-24 10:08:35 +0100

    Tests: Add unit test for interpolation in incremental mode

--- test/unit/test_ResolutionProofInterpolation.cc
+++ test/unit/test_ResolutionProofInterpolation.cc
@@ -78,3 +78,51 @@ TEST_F(ResolutionProofInterpolationTest, test_McMillanPrimeInterpolant) {
     ASSERT_TRUE(verifyInterpolant(A_part, B_part, itp));
 }
 
+
+
+/*************** TESTS FOR INTERPOLATION FOR INCREMENTAL SOLVING *****************/
+class ResolutionProofIncrementalInterpolationTest : public ::testing::Test {
+protected:
+    ResolutionProofIncrementalInterpolationTest(): logic{opensmt::Logic_t::QF_UF} {}
+    virtual void SetUp() {
+        const char* msg;
+        config.setOption(SMTConfig::o_produce_inter, SMTOption(true), msg);
+        solver = std::make_unique<MainSolver>(logic, config, "test");
+        a = logic.mkBoolVar("a");
+        b = logic.mkBoolVar("c");
+        c = logic.mkBoolVar("b");
+        na = logic.mkNot(a);
+        nb = logic.mkNot(b);
+        nc = logic.mkNot(c);
+
+        A_part = logic.mkAnd(logic.mkOr(a, nb), logic.mkOr(a, b));
+        B_part = logic.mkAnd(logic.mkOr(na, nc), logic.mkOr(na, c));
+
+        solver->push();
+        solver->insertFormula(A_part);
+        solver->push();
+        solver->insertFormula(B_part);
+        auto res = solver->check();
+        ASSERT_EQ(res, s_False);
+    }
+    Logic logic;
+    SMTConfig config;
+    std::unique_ptr<MainSolver> solver;
+    PTRef a, b, c, na, nb, nc;
+    PTRef A_part, B_part;
+
+    bool verifyInterpolant(PTRef A, PTRef B, PTRef itp) {
+        return VerificationUtils(config, logic).verifyInterpolantInternal(A, B, itp);
+    }
+};
+
+TEST_F(ResolutionProofIncrementalInterpolationTest, test_McMillanInterpolant) {
+    config.setBooleanInterpolationAlgorithm(itp_alg_mcmillan);
+    auto itpContext = solver->getInterpolationContext();
+    vec<PTRef> itps;
+    ipartitions_t A_mask = 1;
+    itpContext->getSingleInterpolant(itps, A_mask);
+    PTRef itp = itps.last();
+//    std::cout << logic.pp(itp);
+    ASSERT_TRUE(verifyInterpolant(A_part, B_part, itp));
+}
