c6aea615d55ae70fb36f3fa44da352f3d8fbaa99
BritikovKI <BritikovKI@yandex.ru>
2022-02-25 18:08:20 +0100

    parser added

--- reparser.sh
+++ reparser.sh
@@ -1,10 +1,53 @@
 #bash!
-for FILE in $1;
+symbols='abcdefghijklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVYXWZ'  # Stupid SO: '
+count_symbols=${#symbols}
+(( length = RANDOM % 6 + 10 ))
+assert_name=""
+asserts=0
+for i in $(seq 1 $length) ; do
+    assert_name+=${symbols:RANDOM % count_symbols:1}
+done
+
+
+i=1;
+j=0;
+assert_regex="\(assert .*\)"
+for FILE in *.smt2;
 do
-    echo -e "$FILE\nLoops Rule\!" > $FILE;
+    sed -e '1 s/\(.*\)/(set-option :produce-interpolants true)\n\1/g' $FILE > temp.txt;
+    cat temp.txt > $FILE;
+    sed -e '/(check-sat)/ s/\(.*\)/\1\n(get-interpolants (and ) (and ))\n/g' $FILE > temp.txt;
+    cat temp.txt > $FILE;
+    echo $FILE
+    asserts=0
+    i=1
     while IFS= read -r line
     do
-      echo "$line"
-    done << $FILE
-
+      if [[ "$line" =~ $assert_regex ]]
+      then
+        asserts=1
+        echo $i;
+#        echo $line;
+        assert_name=""
+        (( length = RANDOM % 6 + 10 ))
+        for j in $(seq 1 $length) ; do
+            assert_name+=${symbols:RANDOM % count_symbols:1}
+        done
+        sed -e "$i s/(assert \(.*\))/(assert (! \1 :named $assert_name ))/g" $FILE > temp.txt;
+        cat temp.txt > $FILE;
+        if [ $((i%2)) == 1 ]
+        then
+          sed -e "/(get-interpolants/ s/(get-interpolants (\(.*\)) (\(.*\)))/(get-interpolants (\1$assert_name ) (\2))/g" $FILE > temp.txt;
+        else
+          sed -e "/(get-interpolants/ s/(get-interpolants (\(.*\)) (\(.*\)))/(get-interpolants (\1) (\2$assert_name ))/g" $FILE > temp.txt;
+        fi
+        cat temp.txt > $FILE;
+        let i++;
+      fi
+#      echo $line;
+      if [ $asserts == 0 ]
+      then
+        let i++;
+      fi
+    done < $FILE
 done
--- res1.smt2
+++
@@ -1,3 +0,0 @@
-(set-logic QF_UF)
-(assert false)
-(check-sat)
