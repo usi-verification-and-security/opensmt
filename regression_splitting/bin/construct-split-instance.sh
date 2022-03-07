05f9cc303df7d8b88ecca062ee8b5f85811060e7
Antti Hyvärinen <antti.hyvarinen@gmail.com>
2022-03-07 14:14:03 +0100

    Splitting regression: scripts for regression with actual solving

---
+++ regression_splitting/bin/construct-split-instance.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+usage="Usage: $0 [-h] -i <base> -s <split> -o <output>"
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -h|--help)
+            echo "$usage"
+            exit 1
+            ;;
+        -i|--input)
+            base=$2
+            ;;
+        -s|--split)
+            split=$2
+            ;;
+        -o|--output)
+            output=$2
+            ;;
+        -*)
+            echo "Error: invalid option '$1'"
+            exit 1
+            ;;
+        *)
+            break
+    esac
+    shift; shift
+done
+
+if [ -z $base ] || [ ! -f $base ]; then
+    echo "Basefile not provided or not accessible: $base"
+    echo $usage
+    exit 1
+fi
+
+if [ -z $split ] || [ ! -f $split ]; then
+    echo "Split file not provided or not accessible: $split"
+    echo $usage
+    exit $1
+fi
+
+if [ -z $output ]; then
+    echo "Outputfile not provided"
+    output=/dev/stdout
+    echo "Using $output"
+fi
+
+bunzip2 -c $base |grep -v '^(check-sat)\|(exit)$' > $output
+cat $split >> $output
+cat << __EOF__ >> $output
+(check-sat)
+(exit)
+__EOF__
+
+
---
+++ regression_splitting/bin/split-and-solve.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+INSTANCECONSTRUCT=./bin/process-instance.sh
+SPLITSOLVER=./bin/solve-split-instance.sh
+
+# Note: symlinkPath needs to match with the output directory in the instance
+symlinkPath=split_and_solve_work
+
+usage="Usage: $0 [-h] -b <solver> -i <input> -p <patch>"
+
+if [ -e $symlinkPath ]; then
+    echo "Symbolic link path exists.  Please remove '$symlinkPath'"
+    echo "$usage"
+    exit 1
+fi
+
+
+TMPDIR=$(mktemp -d)
+
+# Note: set to `true' to prevent deletion of temporary files
+keep=true
+trap "if [ x$keep == xtrue ]; then echo 'keeping output in ${TMPDIR}'; else rm -rf ${TMPDIR}; fi" EXIT
+
+function constructInstance () {
+    smtfile=$1
+    patch=$2
+    inputfile=${TMPDIR}/input.smt2
+    ${INSTANCECONSTRUCT} \
+        -i ${smtfile} \
+        -p ${patch} \
+        -o ${inputfile}
+    echo $inputfile
+}
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -h|--help)
+            echo "$usage"
+            exit 1
+            ;;
+        -b|--binary)
+            solver=$2
+            ;;
+        -i|--input)
+            base=$2
+            ;;
+        -p|--patch)
+            patch=$2
+            ;;
+        -*)
+            echo "Error: invalid option '$1'"
+            exit 1
+            ;;
+        *)
+            break
+    esac
+    shift; shift
+done
+
+if [ -z $solver ] || [ ! -f $solver ]; then
+    echo "Solver not provided or does not exist: '$solver'"
+    echo $usage
+    exit 1
+fi
+
+if [ -z $base ] || [ ! -f $base ]; then
+    echo "Basefile not provided or not accessible: '$base'"
+    echo $usage
+    exit 1
+fi
+
+if [ -z $patch ] || [ ! -f $patch ]; then
+    echo "Patch file not provided or not accessible: '$patch'"
+    echo $usage
+    exit $1
+fi
+
+mkdir $TMPDIR/$symlinkPath
+ln -s $TMPDIR/$symlinkPath $symlinkPath
+
+file=$(constructInstance $base $patch)
+
+echo "Running $solver on $file"
+
+solverResult=$($solver $file)
+
+if [ x$solverResult != x"unknown" ]; then
+    echo $solverResult
+fi
+
+numSplits=$(ls $symlinkPath/*.smt2 |wc -l)
+
+if [ $numSplits -eq 0 ]; then
+    echo "Error: no splits found but result was unknown"
+    exit 1
+fi
+
+$SPLITSOLVER -i $base -s $symlinkPath -b $solver
+
+if [ x$keep != xtrue ]; then
+    rm $symlinkPath
+fi
+
