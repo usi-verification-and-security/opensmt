d902481ded34ceb8d1fc05d6f91782a1c66cc44b
Antti Hyvärinen <antti.hyvarinen@gmail.com>
2022-03-22 11:41:36 +0100

    cmake: Error if static libraries not found
    If the user requests a "maximally static build", give an error if the
    libraries found do not match with `\.a$`.
    
    In addition, disable from the build things that likely won't work: do
    not build the dynamic library and do not build unit tests as they link
    against the dynamic library.

--- cmake_modules/FindGMP.cmake
+++ cmake_modules/FindGMP.cmake
@@ -3,11 +3,19 @@
 # GMP_INCLUDE_DIR - the GMP include directory
 # GMP_LIBRARIES - Libraries needed to use GMP
 
-if(PREFER_STATIC_GMP)
+if(FORCE_STATIC_GMP)
     #message("Trying to find static version of GMP")
-    find_library(GMP_LIBRARY NAMES libgmp.a)
-    find_library(GMPXX_LIBRARY NAMES libgmpxx.a)
-endif(PREFER_STATIC_GMP)
+    find_library(GMP_LIBRARY NAMES libgmp.a REQUIRED)
+    find_library(GMPXX_LIBRARY NAMES libgmpxx.a REQUIRED)
+    if(NOT ${GMP_LIBRARY} MATCHES ".*\.a$" OR NOT ${GMPXX_LIBRARY} MATCHES ".*\.a$")
+        MESSAGE(WARNING
+                "Failed to find static libraries for GMP or GMPXX.\n"
+                "Hint: Try clearing CMakeCache.txt and request static libs on command line.  "
+                "cmake will not switch to static libraries if it was previously configured "
+                "with shared libraries. ")
+        MESSAGE(SEND_ERROR "Static GMP libs were not found")
+    endif()
+endif(FORCE_STATIC_GMP)
 
 find_library(GMP_LIBRARY NAMES gmp)
 find_library(GMPXX_LIBRARY NAMES gmpxx)
