a41b112c15f3ff2706f9f4d8599b7c3778b8fc41
Antti Hyvärinen <antti.hyvarinen@gmail.com>
2022-03-22 15:45:47 +0100

    ci: static binaries
    Adds a continuous integration script for building static libraries
    and changes the starexec release build in circleci to run the
    static binary production.
    
    The script aims at facilitating release builds and therefore also
    strips and compresses the resulting opensmt binary.

---
+++ ci/build_maximally_static.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+if [ -d build ]; then rm -rf build; fi
+mkdir -p build
+cd build
+
+if [ ! -z ${CMAKE_CXX_COMPILER} ]; then
+    COMPILER_OPTION="-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}"
+fi
+
+cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+      -DCMAKE_CXX_FLAGS="${FLAGS}" \
+      -DUSE_READLINE:BOOL=${USE_READLINE} \
+      -DENABLE_LINE_EDITING:BOOL=${ENABLE_LINE_EDITING} \
+      -DCMAKE_INSTALL_PREFIX=${OSMT_INSTALL} \
+      -DMAXIMALLY_STATIC_BINARY=YES\
+      -DPACKAGE_BENCHMARKS=${PACKAGE_BENCHMARKS} \
+      ${COMPILER_OPTION} \
+      ..
+
+make -j4
+
+strip opensmt
+
+tar jcf opensmt.tar.bz2 opensmt
+
+echo "Placed stripped, maximally static binary in $(pwd)/opensmt.tar.bz2"
