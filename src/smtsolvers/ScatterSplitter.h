0478015570b1c1f2b555af41e3885099c8b26aa6
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-04-08 22:09:05 +0200

    ScatterSplitter: Clause Learning Operation by storing the branch(address) to which the learned clause belongs and communicate that as-is to the solver

--- src/smtsolvers/ScatterSplitter.h
+++ src/smtsolvers/ScatterSplitter.h
@@ -11,7 +11,9 @@
 #include "SimpSMTSolver.h"
 #include "SplitData.h"
 #include "SplitContext.h"
+
 #include <PTPLib/net/Channel.hpp>
+#include <PTPLib/common/Exception.hpp>
 
 class ScatterSplitter : public SimpSMTSolver {
 public:
@@ -19,14 +21,24 @@ public:
 
     std::vector<SplitData> const & getSplits() { return splitContext.getSplits(); }
 
+    vec<opensmt::pair<int,int>> const &  get_solver_branch()  const  { return solverBranch; }
+
+    void set_solver_branch(std::string solver_branch);
+
 private:
-    std::vector<vec<Lit>> split_assumptions;
-    SplitContext splitContext;
-    PTPLib::net::Channel & channel;
+    std::vector<vec<Lit>>   split_assumptions;
+    SplitContext            splitContext;
+    PTPLib::net::Channel &  channel;
+    int                     trail_sent;
+    bool                    firstPropagation;
+    int                     numTriviallyPropagatedOnDl0;
+    vec<opensmt::pair<int,int>> solverBranch;
 
     bool     scatterLevel();                                                  // Are we currently on a scatter level.
     opensmt::pair<SplitData,lbool> createSplitAndBlockAssumptions();          // Create a split formula and place it to the splits vector.
     bool     excludeAssumptions(vec<Lit> const & neg_constrs);                // Add a clause to the database and propagate
+
+    void shallLearnClauses () override ;                                      // Check if solver is in clause share mode
 
 protected:
     lbool solve_() override;
@@ -36,6 +48,27 @@ protected:
     ConsistencyAction notifyConsistency() override;
     void notifyEnd() override;
     lbool zeroLevelConflictHandler() override;                                // Common handling of zero-level conflict as it can happen at multiple places
+
+    bool learnSomeClauses(std::vector<PTPLib::net::Lemma> & learnedLemmas);
+
+    inline bool isAssumptionVar(Var  v)   const &   { return theory_handler.getTMap().isAssumptionVar(v); }
+
+    inline vec<opensmt::pair<int,int>> const & get_solverBranch(Var v) const {
+        return theory_handler.getTMap().get_solverBranch(theory_handler.getTMap().get_FrameId(v));
+    }
+
+    int getAssumptionLevel(Var v) const;
+
+    inline bool isPrefix(const vec<opensmt::pair<int,int>> &  prefix, const vec<opensmt::pair<int,int>> &  full)
+    {
+        if (prefix.size() > full.size())
+            return false;
+        for (int i = 0; i < prefix.size(); ++i) {
+            if (prefix[i].first != full[i].first or prefix[i].second != full[i].second)
+                return false;
+        }
+        return true;
+    }
 };
 
 
