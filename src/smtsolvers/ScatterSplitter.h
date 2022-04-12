d5d9fd908f0f02c8d56614bcac40b91a9a321366
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-04-12 19:30:08 +0200

    [ci skip] ScatterSplitter: single query version of scatter splitter

--- src/smtsolvers/ScatterSplitter.h
+++ src/smtsolvers/ScatterSplitter.h
@@ -12,42 +12,17 @@
 #include "SplitData.h"
 #include "SplitContext.h"
 
-#include <PTPLib/net/Channel.hpp>
-#include <PTPLib/common/Exception.hpp>
-#include <PTPLib/common/Printer.hpp>
-
 class ScatterSplitter : public SimpSMTSolver {
 public:
-    ScatterSplitter(SMTConfig & c, THandler & t, PTPLib::net::Channel & ch);
+    ScatterSplitter(SMTConfig & c, THandler & t);
 
     std::vector<SplitData> const & getSplits() { return splitContext.getSplits(); }
-
-    vec<opensmt::pair<int,int>> const &  get_solver_branch()  const  { return solverBranch; }
-
-    void set_solver_branch(std::string solver_branch);
-
-    PTPLib::net::Channel & getChannel() const { return channel;};
-
-    void passRef_SyncedStream(PTPLib::common::synced_stream & ss) { syncedStream = &ss; }
-
 private:
-    std::vector<vec<Lit>>   split_assumptions;
-    SplitContext            splitContext;
-    PTPLib::net::Channel &  channel;
-    int                     trail_sent;
-    bool                    firstPropagation;
-    int                     numTriviallyPropagatedOnDl0;
-    vec<opensmt::pair<int,int>> solverBranch;
-    PTPLib::common::synced_stream * syncedStream;
-
+    std::vector<vec<Lit>> split_assumptions;
+    SplitContext splitContext;
     bool     scatterLevel();                                                  // Are we currently on a scatter level.
     opensmt::pair<SplitData,lbool> createSplitAndBlockAssumptions();          // Create a split formula and place it to the splits vector.
     bool     excludeAssumptions(vec<Lit> const & neg_constrs);                // Add a clause to the database and propagate
-
-    void shallLearnClauses () override;                                       // Check if solver is in clause share mode or single-query mode and then starts clause learning operation
-
-    void notifyFoundedResult(lbool const & result) const;                     // OpenSMT signalling to communication channel that it founded a result and will stop
-
 protected:
     lbool solve_() override;
     bool branchLitRandom() override;
@@ -56,27 +31,6 @@ protected:
     ConsistencyAction notifyConsistency() override;
     void notifyEnd() override;
     lbool zeroLevelConflictHandler() override;                                // Common handling of zero-level conflict as it can happen at multiple places
-
-    bool learnSomeClauses(std::vector<PTPLib::net::Lemma> & learnedLemmas);
-
-    inline bool isAssumptionVar(Var  v)   const &   { return theory_handler.getTMap().isAssumptionVar(v); }
-
-    inline vec<opensmt::pair<int,int>> const & get_solverBranch(Var v) const {
-        return theory_handler.getTMap().get_solverBranch(theory_handler.getTMap().get_FrameId(v));
-    }
-
-    int getAssumptionLevel(Var v) const;
-
-    inline bool isPrefix(const vec<opensmt::pair<int,int>> &  prefix, const vec<opensmt::pair<int,int>> &  full)
-    {
-        if (prefix.size() > full.size())
-            return false;
-        for (int i = 0; i < prefix.size(); ++i) {
-            if (prefix[i].first != full[i].first or prefix[i].second != full[i].second)
-                return false;
-        }
-        return true;
-    }
 };
 
 
