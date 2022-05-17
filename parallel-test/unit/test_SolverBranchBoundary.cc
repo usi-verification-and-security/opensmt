3cb233d5a573cf3ca495d4bc68f76e0e1107a615
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-05-17 20:54:50 +0200

    parallel - unitTest: have a separate parallel-test/unit folder for parallel tests

--- parallel-test/unit/test_SolverBranchBoundary.cc
+++ parallel-test/unit/test_SolverBranchBoundary.cc
@@ -8,6 +8,7 @@
 #include <gtest/gtest.h>
 #include <Logic.h>
 #include <MainSplitter.h>
+#include <PTPLib/net/Channel.hpp>
 
 class SolverBranchBoundary : public ::testing::Test {
 public:
@@ -18,10 +19,11 @@ public:
 
 TEST_F(SolverBranchBoundary, test_SolverBranch) {
 
+    auto channel = std::make_unique<PTPLib::net::Channel>();
     auto th = MainSolver::createTheory(logic, config);
     auto tm = std::make_unique<TermMapper>(logic);
     auto thandler = new THandler(*th, *tm);
-    std::unique_ptr<SimpSMTSolver> smt_solver = std::make_unique<ParallelScatterSplitter>(config, *thandler);
+    std::unique_ptr<SimpSMTSolver> smt_solver = std::make_unique<ScatterSplitter>(config, *thandler, *channel);
     std::unique_ptr<MainSplitter> mainSplitter = std::make_unique<MainSplitter>(std::move(th),
                                           std::move(tm),
                                           std::unique_ptr<THandler>(thandler),
@@ -40,19 +42,20 @@ TEST_F(SolverBranchBoundary, test_SolverBranch) {
         var(mkLit(5, true)),
         var(mkLit(6, true)),
     };
-
-    (dynamic_cast<ParallelScatterSplitter&>(mainSplitter->getSMTSolver())).set_solver_branch("0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1");
+    std::string solver_branch = std::string("0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1");
+    (dynamic_cast<ScatterSplitter&>(mainSplitter->getSMTSolver())).set_solver_branch(solver_branch);
 
     vec<opensmt::pair<int, int>> const& solverBranch =
-            (dynamic_cast<ParallelScatterSplitter&>(mainSplitter->getSMTSolver())).get_solver_branch();
+            (dynamic_cast<ScatterSplitter&>(mainSplitter->getSMTSolver())).get_solver_branch();
 
     for (int index = 0; index < vars.size(); ++index) {
         int span = index;
         int frameId = index + 1;
-        (dynamic_cast<ParallelScatterSplitter&>(mainSplitter->getSMTSolver())).addBranchToFrameId(opensmt::span<opensmt::pair<int, int> const>(solverBranch.begin(), span), frameId);
+        (dynamic_cast<ScatterSplitter&>(mainSplitter->getSMTSolver())).addBranchToFrameId(opensmt::span<opensmt::pair<int, int> const>(solverBranch.begin(), span), frameId);
         mainSplitter->getTermMapper().mapEnabledFrameIdToVar(vars[index], frameId);
 
-        vec<opensmt::pair<int, int>> const & solverBranch_perVar = (dynamic_cast<ParallelScatterSplitter&>(mainSplitter->getSMTSolver())).get_solverBranch(vars[index]);
+        vec<opensmt::pair<int, int>> const & solverBranch_perVar = (dynamic_cast<ScatterSplitter &>(mainSplitter->getSMTSolver())).getSolverBranch(
+                vars[index]);
         ASSERT_EQ(solverBranch_perVar.size(), span);
     }
 
