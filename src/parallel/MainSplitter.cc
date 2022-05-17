3942a65b1e37a357f7a3090bea62d6ebc30d8de4
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-05-17 17:48:35 +0200

    [ci skip] MainSplitter: Split MainSplitter into cc and .h, and some improvements on the api's

---
+++ src/parallel/MainSplitter.cc
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2022, Antti Hyvarinen <antti.hyvarinen@gmail.com>
+ * Copyright (c) 2022, Seyedmasoud Asadzadeh <seyedmasoud.asadzadeh@usi.ch>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "MainSplitter.h"
+
+void MainSplitter::notifyResult(sstat const & result)
+{
+    if (result != s_Undef) {
+        getChannel().setShallStop();
+        getChannel().notify_all();
+    }
+}
+
+sstat MainSplitter::check() {
+    if (not config.sat_solver_limit()) {
+        //push frames size should match with length of the solver branch
+        if (frames.size() !=
+            static_cast<std::size_t>(getScatterSplitter().get_solver_branch().size() +1))
+            throw PTPLib::common::Exception(__FILE__, __LINE__,
+                                            ";assert: Inconsistency in push frames size and length of the solver address");
+    }
+    sstat res = MainSolver::check();
+    notifyResult(res);
+    status = res;
+    return res;
+}
+
+sstat MainSplitter::solve_(vec<FrameId> & enabledFrames) {
+    if (not config.sat_solver_limit()) {
+        vec<opensmt::pair<int, int>> const & solverBranch = getScatterSplitter().get_solver_branch();
+        if (enabledFrames.size() > solverBranch.size() + 1) {
+            throw PTPLib::common::Exception(__FILE__, __LINE__,
+                                            ";assert: inconsistency in solverBranch length and enabled_frame size: " +
+                                            std::to_string(enabledFrames.size()) + " solverBranch size: " +
+                                            std::to_string(solverBranch.size()));
+        }
+        for (int i = 0; i < enabledFrames.size(); i++) {
+            if (i > 0)
+                getScatterSplitter().addBranchToFrameId(opensmt::span<opensmt::pair<int, int> const>(solverBranch.begin(), i), enabledFrames[i].id);
+        }
+    }
+    sstat res = MainSolver::solve_(enabledFrames);
+    if (getSplitter().getSplits().size() >= 1 and res == s_False)
+        throw PTPLib::common::Exception(__FILE__, __LINE__, ";assert: At least one split is created but splitter returned unsatisfiable.");
+    return res;
+}
+
+sstat MainSplitter::solve() {
+    sstat res = MainSolver::solve();
+    if (getSplitter().getSplits().size() >= 1 and res == s_False)
+        throw PTPLib::common::Exception(__FILE__, __LINE__, ";assert: At least one split is created but splitter returned unsatisfiable");
+    return res;
+};
+
+void MainSplitter::writeSplits(std::string const & baseName) const {
+    assert(config.sat_split_type() != spt_none);
+    auto const & splits = getSplitter().getSplits();
+
+    int i = 0;
+    for (auto const &split : splits) {
+        auto conj_vec = addToConjunction(split.constraintsToPTRefs(*thandler));
+        auto problem = logic.mkAnd(conj_vec);
+        auto zeroPadNumber = [](int number, unsigned long targetLength) {
+            std::string s = std::to_string(number);
+            return std::string(targetLength - std::min(targetLength, s.length()), '0') + s;
+        };
+
+        std::string name = baseName + '-' + zeroPadNumber(i++, static_cast<int>(std::log10(splits.size()))+1) + ".smt2";
+        std::ofstream outFile;
+        outFile.open(name);
+        if (outFile.is_open()) {
+            logic.dumpFormulaToFile(outFile, problem);
+            outFile.close();
+        } else {
+            throw OsmtApiException("Failed to open file " + name);
+        }
+    }
+}
+
+std::unique_ptr<SimpSMTSolver> MainSplitter::createInnerSolver(SMTConfig & config, THandler & thandler, PTPLib::net::Channel & ch) {
+    if (config.sat_split_type() == spt_scatter) {
+        return std::make_unique<ScatterSplitter>(config, thandler, ch);
+    } else if (config.sat_split_type() == spt_lookahead) {
+        return std::make_unique<LookaheadSplitter>(config, thandler);
+    } else {
+        return MainSolver::createInnerSolver(config, thandler);
+    }
+}
+
+std::vector<std::string> MainSplitter::getPartitionClauses() {
+    assert(not isSplitTypeNone());
+    std::vector<std::string> partitions;
+    auto const & splits = getSplitter().getSplits();
+    for (auto const &split : splits) {
+        auto conj_vec = addToConjunction(split.constraintsToPTRefs(*thandler));
+        auto problem = logic.mkAnd(conj_vec);
+        partitions.push_back(logic.dumpWithLets(problem));
+    }
+    return partitions;
+}
+
+vec<PTRef> MainSplitter::addToConjunction(std::vector<vec<PtAsgn>> const & in) const {
+    vec<PTRef> out;
+    for (const auto & constr : in) {
+        vec<PTRef> disj_vec;
+        for (const auto & pta : constr)
+            disj_vec.push(pta.sgn == l_True ? pta.tr : logic.mkNot(pta.tr));
+        out.push(logic.mkOr(std::move(disj_vec)));
+    }
+    return out;
+}
--- src/parallel/MainSplitter.h
+++ src/parallel/MainSplitter.h
@@ -5,8 +5,8 @@
  * SPDX-License-Identifier: MIT
  */
 
-#ifndef OPENSMT_MAINSPLITTER_HPP
-#define OPENSMT_MAINSPLITTER_HPP
+#ifndef MAINSPLITTER_H
+#define MAINSPLITTER_H
 
 #include "MainSolver.h"
 #include "SplitData.h"
@@ -18,144 +18,42 @@
 #include <cmath>
 
 class MainSplitter : public MainSolver {
+
+private:
+    inline bool isSplitTypeScatter() const &       { return dynamic_cast<Splitter&>(ts.solver).isSplitTypeScatter(); }
+
+    inline bool isSplitTypeNone()    const &       { return dynamic_cast<Splitter&>(ts.solver).isSplitTypeNone(); }
+
+    inline PTPLib::net::Channel & getChannel()     { return (dynamic_cast<ScatterSplitter&>(getSMTSolver())).getChannel(); }
+
+    inline ScatterSplitter & getScatterSplitter()  { return dynamic_cast<ScatterSplitter&>(getSMTSolver()); }
+
+    inline Splitter & getSplitter()  const         { return dynamic_cast<Splitter&>(ts.solver); }
+
+    void notifyResult(sstat const & result);
+
+    vec<PTRef> addToConjunction(std::vector<vec<PtAsgn>> const &) const;
+
+    sstat solve_(vec<FrameId> & enabledFrames) override;
+
+    sstat check() override;
+
 public:
-
-    bool isSplitTypeScatter() const & { return dynamic_cast<Splitter&>(ts.solver).isSplitTypeScatter(); }
-
-    bool isSplitTypeNone() const & { return dynamic_cast<Splitter&>(ts.solver).isSplitTypeNone(); }
-
     MainSplitter(std::unique_ptr<Theory> t,std::unique_ptr<TermMapper> tm, std::unique_ptr<THandler> th,
                  std::unique_ptr<SimpSMTSolver> ss, Logic & logic, SMTConfig & config, std::string name)
-                 :
-                 MainSolver(std::move(t), std::move(tm), std::move(th), std::move(ss),logic,config, std::move(name))
-                 {}
+            : MainSolver(std::move(t), std::move(tm), std::move(th), std::move(ss),logic,config, std::move(name))
+    {}
 
-    PTPLib::net::Channel & getChannel()  { return (dynamic_cast<ScatterSplitter&>(getSMTSolver())).getChannel(); }
-    ScatterSplitter & getScatterSplitter()  { return dynamic_cast<ScatterSplitter&>(getSMTSolver()) ;}
-    void notifyResult(sstat const & result)
-    {
-        if (result != s_Undef) {
-            getChannel().setShallStop();
-            getChannel().notify_all();
-        }
-    }
+    sstat solve();
 
-    sstat check() override {
-        if (not config.sat_solver_limit()) {
-            //push frames size should match with length of the solver branch
-            if (frames.size() !=
-                static_cast<std::size_t>((dynamic_cast<ScatterSplitter &>(getSMTSolver())).get_solver_branch().size() +1))
-                throw PTPLib::common::Exception(__FILE__, __LINE__,
-                                                ";assert: Inconsistency in push frames size and length of the solver address");
-        }
-        sstat res = MainSolver::check();
-        notifyResult(res);
-        status = res;
-        return res;
-    }
+    std::vector<std::string> getPartitionClauses();
 
-    sstat solve_(vec<FrameId> & enabledFrames) override {
-        if (not config.sat_solver_limit()) {
-            vec<opensmt::pair<int, int>> const & solverBranch = (dynamic_cast<ScatterSplitter &>(getSMTSolver())).get_solver_branch();
-            if (enabledFrames.size() > solverBranch.size() + 1) {
-                throw PTPLib::common::Exception(__FILE__, __LINE__,
-                                                ";assert: inconsistency in solverBranch length and enabled_frame size: " +
-                                                std::to_string(enabledFrames.size()) + " solverBranch size: " +
-                                                std::to_string(solverBranch.size()));
-            }
-            for (int i = 0; i < enabledFrames.size(); i++) {
-                if (i > 0) {
-                    (dynamic_cast<ScatterSplitter &>(getSMTSolver())).addBranchToFrameId(
-                            opensmt::span<opensmt::pair<int, int> const>(solverBranch.begin(), i), enabledFrames[i].id);
-                }
-            }
-        }
-        sstat res = MainSolver::solve_(enabledFrames);
-        if (dynamic_cast<Splitter&>(ts.solver).getSplits().size() >= 1 and res == s_False)
-            throw PTPLib::common::Exception(__FILE__, __LINE__, ";assert: At least one split is created but splitter returned unsatisfiable.");
-        return res;
-    }
+    void writeSplits(std::string const &)  const;
 
+    static std::unique_ptr<SimpSMTSolver> createInnerSolver(SMTConfig &, THandler &, PTPLib::net::Channel &);
 
-    sstat solve() {
-        sstat res = MainSolver::solve();
-        if (dynamic_cast<Splitter&>(ts.solver).getSplits().size() >= 1 and res == s_False)
-            throw PTPLib::common::Exception(__FILE__, __LINE__, ";assert: At least one split is created but splitter returned unsatisfiable");
-        return res;
-    };
-
-    void writeSplits(std::string const & baseName) const {
-        assert(config.sat_split_type() != spt_none);
-        std::vector<SplitData> const & splits = dynamic_cast<Splitter&>(ts.solver).getSplits();
-
-        int i = 0;
-        auto addToConjunction = [this](std::vector<vec<PtAsgn>> const & in) {
-            vec<PTRef> out;
-            for (const auto & constr : in) {
-                vec<PTRef> disj_vec;
-                for (PtAsgn pta : constr)
-                    disj_vec.push(pta.sgn == l_True ? pta.tr : logic.mkNot(pta.tr));
-                out.push(logic.mkOr(std::move(disj_vec)));
-            }
-            return out;
-        };
-
-        for (auto const &split : splits) {
-            vec<PTRef> conj_vec = addToConjunction(split.constraintsToPTRefs(*thandler));
-
-            PTRef problem = logic.mkAnd(conj_vec);
-
-            auto zeroPadNumber = [](int number, unsigned long targetLength) {
-                std::string s = std::to_string(number);
-                return std::string(targetLength - std::min(targetLength, s.length()), '0') + s;
-            };
-
-            std::string name = baseName + '-' + zeroPadNumber(i++, static_cast<int>(std::log10(splits.size()))+1) + ".smt2";
-            std::ofstream outFile;
-            outFile.open(name);
-            if (outFile.is_open()) {
-                logic.dumpFormulaToFile(outFile, problem);
-                outFile.close();
-            } else {
-                throw OsmtApiException("Failed to open file " + name);
-            }
-        }
-    }
-
-    static std::unique_ptr<SimpSMTSolver> createInnerSolver(SMTConfig & config, THandler & thandler, PTPLib::net::Channel & ch) {
-        if (config.sat_split_type() == spt_scatter) {
-            return std::make_unique<ScatterSplitter>(config, thandler, ch);
-        } else if (config.sat_split_type() == spt_lookahead) {
-            return std::make_unique<LookaheadSplitter>(config, thandler);
-        } else {
-            return MainSolver::createInnerSolver(config, thandler);
-        }
-    }
-
-    std::vector<std::string> getPartitionClauses() {
-        assert(not isSplitTypeNone());
-        std::vector<std::string> partitions;
-        std::vector<SplitData> const & splits = dynamic_cast<Splitter&>(ts.solver).getSplits();
-        auto addToConjunction = [this](std::vector<vec<PtAsgn>> const & in) {
-            vec<PTRef> out;
-            for (const auto & constr : in) {
-                vec<PTRef> disj_vec;
-                for (PtAsgn pta : constr)
-                    disj_vec.push(pta.sgn == l_True ? pta.tr : logic.mkNot(pta.tr));
-                out.push(logic.mkOr(std::move(disj_vec)));
-            }
-            return out;
-        };
-        for (auto const &split : splits) {
-            vec<PTRef> conj_vec = addToConjunction(split.constraintsToPTRefs(*thandler));
-            PTRef problem = logic.mkAnd(conj_vec);
-            partitions.push_back(logic.dumpWithLets(problem));
-        }
-        return partitions;
-    }
-
-    TermMapper& getTermMapper() { return *term_mapper;}
+    inline TermMapper& getTermMapper() const { return *term_mapper;}
 };
 
 
-#endif //OPENSMT_MAINSPLITTER_HPP
+#endif //MAINSPLITTER_H
