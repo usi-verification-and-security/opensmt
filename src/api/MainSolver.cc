4394a5503dfae3cf72bd8d0c4e63c8a2f9941933
Konstantin <BritikovKI@Yandex.ru>
2023-04-14 09:12:37 +0200

    Updated Core to be pure lookahead in progress

--- src/api/MainSolver.cc
+++ src/api/MainSolver.cc
@@ -317,7 +317,7 @@ std::unique_ptr<SimpSMTSolver> MainSolver::createInnerSolver(SMTConfig & config,
     } else if (config.use_ghost_vars()) {
         return std::make_unique<GhostSMTSolver>(config, thandler);
     } else if (config.sat_picky()) {
-    return std::make_unique<LookaheadSMTSolver>(config, thandler);
+        return std::make_unique<SimpSMTSolver>(config, thandler);
     } else {
         return std::make_unique<SimpSMTSolver>(config, thandler);
     }
