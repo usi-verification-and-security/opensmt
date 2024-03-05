fa1c1546bedd2d221e86143d13a79db1400da54a
Martin Blicha <martin.blicha@gmail.com>
2024-03-05 18:22:41 +0100

    Update license to MIT in proof directory
    These files originally belonged to Periplo, orginally a sibling project
    to OpenSMT. It was eventually incorporated into OpenSMT and this
    happened a long time ago. We update the license to match OpenSMT's
    license.

--- src/proof/PG.h
+++ src/proof/PG.h
@@ -1,21 +1,9 @@
-/*********************************************************************
-Author: Simone Fulvio Rollini <simone.rollini@gmail.com>
-
-Periplo -- Copyright (C) 2013, Simone Fulvio Rollini
-
-Periplo is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Periplo is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Periplo. If not, see <http://www.gnu.org/licenses/>.
- *********************************************************************/
+/*
+ *  Copyright (c) 2013, Simone Fulvio Rollini <simone.rollini@gmail.com>
+ *
+ *  SPDX-License-Identifier: MIT
+ *
+ */
 
 #ifndef PROOFGRAPH_H
 #define PROOFGRAPH_H
