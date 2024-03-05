534c5132337c1dba410b152605d1d232aa83032d
Martin Blicha <martin.blicha@gmail.com>
2024-03-05 19:41:27 +0100

    Tests: Remove old unused files

--- test/Snode.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-#include <list>
-#include <vector>
-
-
--- test/test.cc
+++
@@ -1,7 +0,0 @@
-#include <vector>
-#include <string>
-
-vector<string> * 
-
-int main(int argc, char ** argv)
-    vector<string> 
