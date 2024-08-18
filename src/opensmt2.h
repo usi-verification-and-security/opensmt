297cf277ef6e8e5cc0a4e62c6cb82b9fd7def5c3
Tomáš Kolárik <tomaqa@gmail.com>
2024-08-18 21:04:14 +0200

    Installing headers into subdirectories

--- src/opensmt2.h
+++ src/opensmt2.h
@@ -1,9 +1,1 @@
-#include <opensmt/Opensmt.h>
-/*
-#include <opensmt/SMTConfig.h>
-#include <opensmt/Theory.h>
-#include <opensmt/Logic.h>
-#include <opensmt/THandler.h>
-#include <opensmt/MainSolver.h>
-#include <opensmt/SimpSMTSolver.h>
-*/
+#include <opensmt/api/Opensmt.h>
