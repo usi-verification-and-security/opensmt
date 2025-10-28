27f0ef648d8b8ea9cfbba26a1c73027d09f27457
Tomáš Kolárik <tomaqa@gmail.com>
2025-10-28 17:22:52 +0100

    Extended ScopedVector of std. container types, clear and accessing no. scopes

--- src/common/ScopedVector.h
+++ src/common/ScopedVector.h
@@ -18,29 +18,39 @@ class ScopedVector {
     std::vector<unsigned> limits;
 
 public:
+    using value_type = T;
+    using reference = T &;
+    using const_reference = T const &;
+    using pointer = T *;
+    using const_pointer = T const *;
+
+    using size_type = std::size_t;
+
     using iterator = typename decltype(elements)::iterator;
     using const_iterator = typename decltype(elements)::const_iterator;
 
     void push(T const & element) { return elements.push_back(element); }
 
     void pushScope() { limits.push_back(elements.size()); }
-
-    void popScope();
-
+    inline void popScope();
     template<typename TFun>
-    void popScope(TFun callback);
+    inline void popScope(TFun callback);
+
+    inline void clear();
 
     [[nodiscard]] bool empty() const { return elements.empty(); }
     [[nodiscard]] std::size_t size() const { return elements.size(); }
-
-    [[nodiscard]] T * data() { return elements.data(); }
-    [[nodiscard]] T const * data() const { return elements.data(); }
 
     [[nodiscard]] auto begin() const { return elements.begin(); }
     [[nodiscard]] auto end() const { return elements.end(); }
 
     [[nodiscard]] auto begin() { return elements.begin(); }
     [[nodiscard]] auto end() { return elements.end(); }
+
+    [[nodiscard]] T const * data() const { return elements.data(); }
+    [[nodiscard]] T * data() { return elements.data(); }
+
+    [[nodiscard]] std::size_t scopeCount() const { return limits.size() + 1; }
 };
 
 template<typename T>
@@ -59,6 +69,12 @@ void ScopedVector<T>::popScope(TFun callback) {
         callback(elements.back());
         elements.pop_back();
     }
+}
+
+template<typename T>
+void ScopedVector<T>::clear() {
+    elements.clear();
+    limits.clear();
 }
 } // namespace opensmt
 
