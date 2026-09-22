b004931a3a0bce6173b1b030928279e13192133f
Tomáš Kolárik <tomaqa@gmail.com>
2026-09-22 18:15:49 +0200

    More mutex lock calls for the timeout mechanism

--- src/api/MainSolver.cc
+++ src/api/MainSolver.cc
@@ -660,6 +660,8 @@ void MainSolver::TimeLimitImpl::setLimit(std::chrono::milliseconds limit) {
         waitToEnd();
     }
 
+    std::lock_guard lock(mtx);
+
     thread = decltype(thread){[this, limit] {
         std::unique_lock lock(mtx);
         // Abort if a further future end request has already been sent
@@ -676,6 +678,7 @@ void MainSolver::TimeLimitImpl::setLimitIfNotRunning(std::chrono::milliseconds l
 }
 
 bool MainSolver::TimeLimitImpl::isRunning() const noexcept {
+    std::lock_guard lock(mtx);
     return thread.joinable();
 }
 
@@ -692,7 +695,10 @@ void MainSolver::TimeLimitImpl::requestEnd() {
 void MainSolver::TimeLimitImpl::waitToEnd() {
     assert(isRunning());
     thread.join();
-    endReq = false;
+    {
+        std::lock_guard lock(mtx);
+        endReq = false;
+    }
     assert(not isRunning());
 }
 } // namespace opensmt
